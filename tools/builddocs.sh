1a25aae066819804a627a84fdc7183653491c3ef
vtraag <vincent@traag.net>
2020-03-12 12:05:15 +0100

    Correct roxygen2 approach.
    First compiling the DLL will require a NAMESPACE file. Generating the
    documentation and NAMESPACE using roxygen2 will require again *not* to
    have a NAMESPACE file present. Hence, it first needs to be added, and
    then needs to be removed again, in order to solve this catch-22.

--- tools/builddocs.sh
+++ tools/builddocs.sh
@@ -6,7 +6,13 @@ rsync -avq --exclude=cigraph --exclude=.git --exclude revdep . ${tempdir}/
 
 (
     cd ${tempdir}
-    Rscript -e 'pkgbuild::compile_dll(); roxygen2::update_collate(".")'
+    touch NAMESPACE # Create namespace, otherwise compile_dll will not work
+    echo "Build DLL"
+    Rscript -e 'pkgbuild::compile_dll();'
+    echo "Update collate"
+    Rscript -e 'roxygen2::update_collate(".")'
+    rm NAMESPACE # Remove namespace, otherwise roxygen will not create proper NAMESPACE
+    echo "Generate documentation and NAMESPACE"
     Rscript -e 'library(devtools); document()'
 )
 
