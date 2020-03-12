b47738d517b8c7c01769f924fa8ee38118bfa09e
vtraag <vincent@traag.net>
2020-03-12 14:05:03 +0100

    Revert back to previous setting to double check.

--- tools/builddocs.sh
+++ tools/builddocs.sh
@@ -6,14 +6,9 @@ rsync -avq --exclude=cigraph --exclude=.git --exclude revdep . ${tempdir}/
 
 (
     cd ${tempdir}
-    touch NAMESPACE # Create namespace, otherwise compile_dll will not work
-    echo "Build DLL"
-    Rscript -e 'pkgbuild::compile_dll();'
-    echo "Update collate"
+    rm -rf src
     Rscript -e 'roxygen2::update_collate(".")'
-    rm NAMESPACE # Remove namespace, otherwise roxygen will not create proper NAMESPACE
-    echo "Generate documentation and NAMESPACE"
-    Rscript -e 'library(devtools); document()'
+    Rscript -e 'library(devtools) ; document()'
 )
 
 cp ${tempdir}/DESCRIPTION .
