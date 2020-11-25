7539ac17bb8a89bb674769fbe9c0dfac8294d4bf
Vincent Traag <vincent@traag.net>
2020-11-25 10:23:59 +0100

    getversion.sh: Simplified obtaining a version from git.

--- tools/getversion.sh
+++ tools/getversion.sh
@@ -11,17 +11,10 @@ fi
 thistag=$(git describe --exact-match --tags HEAD 2>/dev/null || true)
 
 if [ -z "${thistag}" ]; then
-    if [ -f $script_dir/NEXT_VERSION ]; then
-        next_version=$( cat $script_dir/NEXT_VERSION )
-        commits=$(git rev-list --count dev..HEAD) # Check commits from dev branch
-        dev=$((9000 + commits))
-        echo "${next_version}.${dev}"
-    else
-        latest_version=$(git describe --abbrev=0 HEAD)
-        commits=$(git rev-list --count $(git describe --tags --abbrev=0)..HEAD)
-        dev=$((9000 + commits))
-        echo "${latest_version}.${dev}"
-    fi
+    latest_version=$(git describe --abbrev=0 HEAD)
+    commits=$(git rev-list --count $(git describe --tags --abbrev=0)..HEAD)
+    dev=$((9000 + commits))
+    echo "${latest_version}.${dev}"
 else
     echo "${thistag}"
 fi
