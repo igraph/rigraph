8bfbf53e7eea4fd3572da05ffe9c69608bdfa141
Claus Hunsen <github@claushunsen.de>
2021-03-07 19:41:44 +0100

    Fix warning due to factors in graph_from_data_frame (#250)

--- R/data_frame.R
+++ R/data_frame.R
@@ -159,7 +159,7 @@ graph_from_data_frame <- function(d, directed=TRUE, vertices=NULL) {
     if (ncol(vertices) > 1) {
       for (i in 2:ncol(vertices)) {
         newval <- vertices[,i]
-        if (class(newval) == "factor") {
+        if (inherits(newval, "factor")) {
           newval <- as.character(newval)
         }
         attrs[[ names(vertices)[i] ]] <- newval
@@ -180,7 +180,7 @@ graph_from_data_frame <- function(d, directed=TRUE, vertices=NULL) {
   if (ncol(d) > 2) {
     for (i in 3:ncol(d)) {
       newval <- d[,i]
-      if (class(newval) == "factor") {
+      if (inherits(newval, "factor")) {
         newval <- as.character(newval)
       }
       attrs[[ names(d)[i] ]] <- newval
