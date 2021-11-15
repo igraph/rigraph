64d17ddf02d3ffc16868dd60cfebd9cc2ca7dbb2
Tamas Nepusz <ntamas@gmail.com>
2021-11-15 20:50:35 +0100

    fix: fix cluster_louvain() test case now that the algorithm because randomized in C-igraph

--- tests/testthat/test_multilevel.community.R
+++ tests/testthat/test_multilevel.community.R
@@ -10,12 +10,12 @@ test_that("cluster_louvain works", {
   mc <- cluster_louvain(g)
   
   expect_that(as.vector(membership(mc)),
-              equals(c(1,1,1,1,2,2,2,1,3,1,2,1,1,1,3,3,2,
+              equals(c(1,2,2,2,1,1,1,2,3,2,1,1,1,2,3,3,1,
                        1,3,1,3,1,3,4,4,4,3,4,4,3,3,4,3,3)))
   expect_that(modularity(g, mc$membership), equals(max(mc$modularity)))
   expect_that(length(mc), equals(4))
   expect_that(sizes(mc),
-              equals(structure(c(12L, 5L, 11L, 6L), .Dim = 4L,
+              equals(structure(c(11L, 6L, 11L, 6L), .Dim = 4L,
                 .Dimnames = structure(list(`Community sizes` = c("1",
                      "2", "3", "4")), .Names = "Community sizes"),
                                class = "table") ))
