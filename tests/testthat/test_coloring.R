56e4878b2bac4f32f757deb93aff1af3f509dddc
Tamas Nepusz <ntamas@gmail.com>
2021-12-30 14:44:44 +0100

    test: added test for greedy_vertex_coloring() when the graph is named

--- tests/testthat/test_coloring.R
+++ tests/testthat/test_coloring.R
@@ -11,3 +11,11 @@ test_that("greedy_vertex_coloring works", {
         equals(c(1, rep(2, vcount(g)-1)))
     )
 })
+
+test_that("greedy_vertex_coloring works on named graphs", {
+    g <- make_star(10, mode="undirected")
+    V(g)$name <- LETTERS[1:vcount(g)]
+    vc <- greedy_vertex_coloring(g)
+    expect_that(as.vector(vc), equals(c(1, rep(2, vcount(g)-1))))
+    expect_that(names(vc), equals(V(g)$name))
+})
