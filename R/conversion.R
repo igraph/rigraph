a80164f1fe6555db37b53cabdd7604fb6fc046b8
Tamas Nepusz <ntamas@gmail.com>
2022-03-30 12:38:07 +0200

    fix: as.matrix() should use the same defaults as as_adjacency_matrix() and as_edgelist()

--- R/conversion.R
+++ R/conversion.R
@@ -933,12 +933,12 @@ as_long_data_frame <- function(graph) {
 #' matrix is returned.  The value of \code{attrname} is ignored.
 #'
 #' If \code{matrix.type} is \code{"adjacency"}, then a square adjacency matrix is
-#' returned. If \code{attrname} is \code{NULL} (default), the matrix is binary.
-#' Otherwise \code{attrname} can be a name of edge attribute of \code{x}. In
-#' that case the cells of the results are the values of that attribute.
+#' returned. For adjacency matrices, you can use the \code{attr} keyword argument
+#' to use the values of an edge attribute in the matrix cells. See the
+#' documentation of \link{as_adjacency_matrix} for more details.
 #'
 #' Other arguments passed through \code{...} are passed to either
-#' \code{\link[igraph]{as_adjacency_matrix}} or \code{\link[igraph]{as_edgelist}}
+#' \code{\link{as_adjacency_matrix}} or \code{\link{as_edgelist}}
 #' depending on the value of \code{matrix.type}.
 #'
 #' @param x object of class igraph, the network
@@ -946,12 +946,11 @@ as_long_data_frame <- function(graph) {
 #' or "edgelist" are supported
 #' @param attrname character, name of the edge attribute to use to fill in the
 #' cells of the adjacency matrix
-#' @param sparse logical, whether to return a sparse matrix
 #' @param \dots other arguments to/from other methods
 #' @return Depending on the value of \code{matrix.type} either a square
 #' adjacency matrix or a two-column numeric matrix representing the edgelist.
 #' @author Michal Bojanowski, originally from the \code{intergraph} package
-#' @seealso \code{\link[igraph]{as_adjacency_matrix}}, \code{\link[igraph]{as_edgelist}}
+#' @seealso \code{\link{as_adjacency_matrix}}, \code{\link{as_edgelist}}
 #' @export
 #' @examples
 #'
@@ -962,10 +961,10 @@ as_long_data_frame <- function(graph) {
 #' E(g)$weight <- rep(1:10, each=ecount(g))
 #' as.matrix(g, "adjacency", sparse=FALSE, attr="weight")
 #'
-as.matrix.igraph <- function(x, matrix.type=c("adjacency", "edgelist"), attrname=NULL, sparse=FALSE, ...) {
+as.matrix.igraph <- function(x, matrix.type=c("adjacency", "edgelist"), ...) {
   mt <- match.arg(matrix.type)
   switch(mt,
-      adjacency = as_adjacency_matrix(graph=x, attr=attrname, sparse=sparse, ...),
+      adjacency = as_adjacency_matrix(graph=x, ...),
       edgelist = as_edgelist(graph=x, ...)
   )
 }
