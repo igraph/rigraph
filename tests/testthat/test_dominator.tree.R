70cf6eee0ddfc7fc0103059f9626aea916f47ed6
Tamas Nepusz <ntamas@gmail.com>
2022-04-04 15:02:55 +0200

    fix: fix `dom' component of the return value of dominator_tree(), closes #186

--- tests/testthat/test_dominator.tree.R
+++ tests/testthat/test_dominator.tree.R
@@ -8,9 +8,13 @@ test_that("dominator_tree works", {
                  K-+I:R, L-+H)
   dtree <- dominator_tree(g, root="R")
 
-  dtree$dom <- V(g)$name[ as.vector(dtree$dom) ]
+  # This is awkward; dtree$dom contains -1 for the root and normal vertex indices
+  # for the rest, and we want to map them to names. This seemed to be the cleanest
+  # way, but it is not nearly as user-friendly as it should be
+  names <- c("$root", V(g)$name)
+  dtree$dom <- names[ifelse(dtree$dom < 0, 1, dtree$dom + 1)]
   dtree$leftout <- V(g)$name[ dtree$leftout ]
-  expect_that(dtree$dom, equals(c("R", "R", "R", "R", "R", "C", "C",
+  expect_that(dtree$dom, equals(c("$root", "R", "R", "R", "R", "R", "C", "C",
                                   "D", "R", "R", "G", "R")))
   expect_that(dtree$leftout, equals(character()))
   expect_that(as_edgelist(dtree$domtree),
