d54e60d225188fec49e02ed7bcd61cba0320f76e
Tamas Nepusz <ntamas@gmail.com>
2022-05-09 15:45:27 +0200

    chore: updated vendored igraph

--- tests/testthat/test_layout.kk.R
+++ tests/testthat/test_layout.kk.R
@@ -3,26 +3,33 @@ context("Kamada-Kawai layouts")
 
 test_that("Kamada-Kawai layout generator works", {
 
-  skip_on_cran()
-  skip_on_ci()
-
   set.seed(42)
+
+  get_radii <- function(layout) {
+    apply(l - colMeans(l), 1, function(x) sqrt(sum(x ** 2)))    
+  }
+
+  sort_by_angles <- function(layout) {
+    angles <- apply(l - colMeans(l), 1, function(x) atan2(x[2], x[1]))
+    layout[order(angles), ]
+  }
+
+  looks_circular <- function(layout, eps = 1e-04) {
+    radii <- get_radii(layout)
+    norm_radii <- (radii - mean(radii)) / mean(radii)
+    layout <- sort_by_angles(layout)
+    dists <- apply(layout[-nrow(layout), ] - layout[-1, ], 1, function(x) sqrt(sum(x ** 2)))
+    norm_dists <- (dists - mean(dists)) / mean(dists)
+    all(abs(norm_radii) < eps) && all(abs(norm_dists) < eps)
+  }
 
   library(igraph)
   g <- make_ring(10)
-  l <- layout_with_kk(g, maxiter=50)
-  if (Sys.info()["sysname"] == "Darwin") {
-    expect_that(sum(l), equals(-1.13071769106689))
-  } else if (Sys.info()["sysname"] == "Linux" &&
-             Sys.info()["machine"] == "x86_64") {
-    expect_that(sum(l), equals(-6.77278645472984e-05))
-  } else if (Sys.info()["sysname"] == "Linux" &&
-             Sys.info()["machine"] == "i686") {
-    expect_that(sum(l), equals(0.914809637353466))
-  }
+  l <- layout_with_kk(g, maxiter=50, coords=layout_in_circle(g))
+  expect_true(looks_circular(l))
 
   g <- make_star(30)
-  l <- layout_with_kk(g, maxiter=500)
+  l <- layout_with_kk(g, maxiter=500, coords=layout_in_circle(g))
   if (Sys.info()["sysname"] == "Darwin" &&
 	  Sys.info()["machine"] == "arm64") {
     expect_that(sum(l), equals(-84.9114513293935))
@@ -38,7 +45,7 @@ test_that("Kamada-Kawai layout generator works", {
 
   g <- make_ring(10)
   E(g)$weight <- rep(1:2, length.out=ecount(g))
-  l <- layout_with_kk(g, maxiter=500)
+  l <- layout_with_kk(g, maxiter=500, coords=layout_in_circle(g))
   if (Sys.info()["sysname"] == "Darwin") {
     expect_that(sum(l), equals(1.61069099387368))
   } else if (Sys.info()["sysname"] == "Linux" &&
@@ -53,9 +60,15 @@ test_that("Kamada-Kawai layout generator works", {
 
 test_that("3D Kamada-Kawai layout generator works", {
 
+  skip_on_cran()
+  skip_on_ci()
+
   library(igraph)
+
+  set.seed(42)
+
   g <- make_star(30)
   l <- layout_with_kk(g, maxiter=5000, dim=3)
-  expect_that(sum(l), equals(61.0559727551764))
+  expect_that(sum(l), equals(-54.8841976017082))
 
 })
