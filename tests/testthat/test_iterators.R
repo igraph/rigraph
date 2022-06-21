4ccd00f777f322b2c3e4002805262cfc8d163155
Tamas Nepusz <ntamas@gmail.com>
2022-06-21 17:09:37 +0200

    feat: added resolve_attrs=... option to single-bracket indexing to turn off attribute lookup

--- tests/testthat/test_iterators.R
+++ tests/testthat/test_iterators.R
@@ -78,3 +78,21 @@ test_that("indexing with characters work as expected", {
   expect_error(E(g)[1:5]['H'], 'Unknown edge selected')
   expect_error(E(g)[6:9]['a|b'], 'Unknown edge selected')
 })
+
+test_that("attribute resolution can be turned off", {
+
+  g <- make_ring(10)
+  V(g)$name <- letters[1:10]
+  E(g)$index <- 10:19
+
+  name <- c("d", "e")
+  index <- 3
+
+  # resolve_attrs is TRUE by default, which means that attribute names take
+  # precedence over local variables
+  expect_equal(as.vector(V(g)[name]), 1:10)
+  expect_error(E(g)[index], 'Unknown edge selected')
+
+  expect_equal(as.vector(V(g)[name, resolve_attrs=FALSE]), c(4, 5))
+  expect_equal(as.vector(E(g)[index, resolve_attrs=FALSE]), 3)
+})
