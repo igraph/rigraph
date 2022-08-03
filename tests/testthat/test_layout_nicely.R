3bd98f43966796e2acc7dd885aee701f96032bff
Tamas Nepusz <ntamas@gmail.com>
2022-08-03 10:41:27 +0200

    fix: test that all layout algorithms work with the null graph

--- tests/testthat/test_layout_nicely.R
+++ tests/testthat/test_layout_nicely.R
@@ -2,12 +2,12 @@ context("Automatic layout")
 
 test_that("layout_nicely() works with negative weights", {
     g <- make_graph("petersen")
-    E(g)$weight <- -5:10
+    E(g)$weight <- -5:9
     expect_warning(layout_nicely(g), regexp = "ignoring all weights")
 })
 
 test_that("layout_nicely() does not recurse into itself", {
     g <- make_graph("petersen")
     g$layout <- layout_nicely
-    layout_nicely(g)   # should not recurse infinitely
+    expect_warning(layout_nicely(g), regexp = NA)   # should not recurse infinitely
 })
