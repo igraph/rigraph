42c1dcbc4d762feffdc02e70ecc0cf21b70b8d58
Kirill Müller <krlmlr+r@mailbox.org>
2022-09-24 05:46:35 +0200

    Avoid require(digest)

--- tests/testthat/test_scan.R
+++ tests/testthat/test_scan.R
@@ -1,5 +1,3 @@
-require(digest)
-
 local_rng_version("3.5.0")
 set.seed(12345)
 n <- 10^3
@@ -11,77 +9,77 @@ E(gp)$weight = sample(ecount(gp))
 
 test_that("General scan-stat works, US, scan-0, unweighted", {
   s1 <- local_scan(g, k=0)
-  expect_that(digest(s1), equals("659ffaaf303742f0806a79b8ff3d88b3"))
+  expect_that(digest::digest(s1), equals("659ffaaf303742f0806a79b8ff3d88b3"))
 })
 
 test_that("General scan-stat works, US, scan-0, weighted", {
   s1 <- local_scan(g, k=0, weighted=TRUE)
-  expect_that(digest(s1), equals("0f8d7ac831389cea04e0bfc5e2510c73"))
+  expect_that(digest::digest(s1), equals("0f8d7ac831389cea04e0bfc5e2510c73"))
 })
 
 
 test_that("General scan-stat works, US, scan-1, unweighted", {
   s1 <- local_scan(g)
-  expect_that(digest(s1), equals("df0fd77489f70cc47f682dc31d9f52f5"))
+  expect_that(digest::digest(s1), equals("df0fd77489f70cc47f682dc31d9f52f5"))
 })
 
 test_that("General scan-stat works, US, scan-1, weighted", {
   s1 <- local_scan(g, k=1, weighted=TRUE)
-  expect_that(digest(s1), equals("af720916ae4b49881745d2dcdd614401"))
+  expect_that(digest::digest(s1), equals("af720916ae4b49881745d2dcdd614401"))
 })
 
 test_that("General scan-stat works, US, scan-2, unweighted", {
   s1 <- local_scan(g, k=2)
-  expect_that(digest(s1), equals("6f47f47abde25d00d615dd56826cca5a"))
+  expect_that(digest::digest(s1), equals("6f47f47abde25d00d615dd56826cca5a"))
 })
 
 test_that("General scan-stat works, US, scan-2, weighted", {
   s1 <- local_scan(g, k=2, weighted=TRUE)
-  expect_that(digest(s1), equals("e02e9d58168ee5d53850497f6d4c76b0"))
+  expect_that(digest::digest(s1), equals("e02e9d58168ee5d53850497f6d4c76b0"))
 })
 
 test_that("General scan-stat works, THEM, scan-0, unweighted", {
   s1 <- local_scan(g, gp, k=0)
-  expect_that(digest(s1), equals("f584f7d287f8f89f5f7882165ca41b8c"))
+  expect_that(digest::digest(s1), equals("f584f7d287f8f89f5f7882165ca41b8c"))
 })
 
 test_that("General scan-stat works, THEM, scan-0, weighted", {
   s1 <- local_scan(g, gp, k=0, weighted=TRUE)
-  expect_that(digest(s1), equals("213db8e7517d1e6406da3dbd55281ed1"))
+  expect_that(digest::digest(s1), equals("213db8e7517d1e6406da3dbd55281ed1"))
 })
 
 test_that("General scan-stat works, THEM, scan-1, unweighted", {
   s1 <- local_scan(g, gp, k=1)
-  expect_that(digest(s1), equals("e9ca740ebba2fd1db4abe939954b2638"))
+  expect_that(digest::digest(s1), equals("e9ca740ebba2fd1db4abe939954b2638"))
 })
 
 test_that("General scan-stat works, THEM, scan-1, weighted", {
   s1 <- local_scan(g, gp, k=1, weighted=TRUE)
-  expect_that(digest(s1), equals("a98e9a03eda7feaae8524dc9348ad74b"))
+  expect_that(digest::digest(s1), equals("a98e9a03eda7feaae8524dc9348ad74b"))
 })
 
 test_that("General scan-stat works, THEM, scan-2, unweighted", {
   s1 <- local_scan(g, gp, k=2)
-  expect_that(digest(s1), equals("a3237a9a55e9d86ab471c81a291eb03b"))
+  expect_that(digest::digest(s1), equals("a3237a9a55e9d86ab471c81a291eb03b"))
 })
 
 test_that("General scan-stat works, THEM, scan-2, weighted", {
   s1 <- local_scan(g, gp, k=2, weighted=TRUE)
-  expect_that(digest(s1), equals("995d0b6a952834ff6e534efc2cfb917b"))
+  expect_that(digest::digest(s1), equals("995d0b6a952834ff6e534efc2cfb917b"))
 })
 
 test_that("Neighborhoods work for us", {
   nei <- neighborhood(g, order=1)
   s1 <- local_scan(g, neighborhoods=nei)
-  expect_that(digest(s1), equals("df0fd77489f70cc47f682dc31d9f52f5"))
+  expect_that(digest::digest(s1), equals("df0fd77489f70cc47f682dc31d9f52f5"))
   s1 <- local_scan(g, k=1, weighted=TRUE, neighborhoods=nei)
-  expect_that(digest(s1), equals("af720916ae4b49881745d2dcdd614401"))
+  expect_that(digest::digest(s1), equals("af720916ae4b49881745d2dcdd614401"))
 
   nei <- neighborhood(g, order=2)
   s1 <- local_scan(g, k=2, neighborhoods=nei)
-  expect_that(digest(s1), equals("6f47f47abde25d00d615dd56826cca5a"))
+  expect_that(digest::digest(s1), equals("6f47f47abde25d00d615dd56826cca5a"))
   s1 <- local_scan(g, k=2, weighted=TRUE, neighborhoods=nei)
-  expect_that(digest(s1), equals("e02e9d58168ee5d53850497f6d4c76b0"))
+  expect_that(digest::digest(s1), equals("e02e9d58168ee5d53850497f6d4c76b0"))
 
 })
 
@@ -89,15 +87,15 @@ test_that("Neighborhoods work for them", {
 
   nei <- neighborhood(g, order=1)
   s1 <- local_scan(g, gp, k=1, neighborhoods=nei)
-  expect_that(digest(s1), equals("e9ca740ebba2fd1db4abe939954b2638"))
+  expect_that(digest::digest(s1), equals("e9ca740ebba2fd1db4abe939954b2638"))
   s1 <- local_scan(g, gp, k=1, weighted=TRUE, neighborhoods=nei)
-  expect_that(digest(s1), equals("a98e9a03eda7feaae8524dc9348ad74b"))
+  expect_that(digest::digest(s1), equals("a98e9a03eda7feaae8524dc9348ad74b"))
 
   nei <- neighborhood(g, order=2)
   s1 <- local_scan(g, gp, k=2, neighborhoods=nei)
-  expect_that(digest(s1), equals("a3237a9a55e9d86ab471c81a291eb03b"))
+  expect_that(digest::digest(s1), equals("a3237a9a55e9d86ab471c81a291eb03b"))
   s1 <- local_scan(g, gp, k=2, weighted=TRUE, neighborhoods=nei)
-  expect_that(digest(s1), equals("995d0b6a952834ff6e534efc2cfb917b"))
+  expect_that(digest::digest(s1), equals("995d0b6a952834ff6e534efc2cfb917b"))
 
 })
 
@@ -115,20 +113,20 @@ E(gp)$weight = sample(ecount(gp))
 test_that("General scan-stat works, US, scan-1, unweighted, directed", {
 
   s1o <- local_scan(g, k=1, weighted=FALSE, mode="out")
-  expect_that(digest(s1o), equals("ac463c21b2b6bc91abf82f0141a4a7d4"))
+  expect_that(digest::digest(s1o), equals("ac463c21b2b6bc91abf82f0141a4a7d4"))
 
   s1i <- local_scan(g, k=1, weighted=FALSE, mode="in")
-  expect_that(digest(s1i), equals("13fdaaeec54118e217821b56d8c3ff03"))
+  expect_that(digest::digest(s1i), equals("13fdaaeec54118e217821b56d8c3ff03"))
 
 })
 
 test_that("General scan-stat works, US, scan-1, weighted, directed", {
 
   s1o <- local_scan(g, k=1, weighted=TRUE, mode="out")
-  expect_that(digest(s1o), equals("da8e14f2ba63efc74b5fd7b9d8f79bbc"))
+  expect_that(digest::digest(s1o), equals("da8e14f2ba63efc74b5fd7b9d8f79bbc"))
 
   s1i <- local_scan(g, k=1, weighted=TRUE, mode="in")
-  expect_that(digest(s1i), equals("f5f07eebb907ae0a244195a20971be11"))
+  expect_that(digest::digest(s1i), equals("f5f07eebb907ae0a244195a20971be11"))
 
 })
 
