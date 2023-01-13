f221ba26e3520271276e7a04a60fb083b8f1169b
Kirill Müller <krlmlr+r@mailbox.org>
2023-01-13 04:00:09 +0100

    Unrelated: break long documentation line

--- R/centrality.R
+++ R/centrality.R
@@ -1217,7 +1217,10 @@ bonpow.sparse <- function(graph, nodes = V(graph), loops = FALSE,
 #' g.c <- graph(c(1, 2, 1, 3, 2, 4, 3, 5), dir = FALSE)
 #' g.d <- graph(c(1, 2, 1, 3, 1, 4, 2, 5, 3, 6, 4, 7), dir = FALSE)
 #' g.e <- graph(c(1, 2, 1, 3, 1, 4, 2, 5, 2, 6, 3, 7, 3, 8, 4, 9, 4, 10), dir = FALSE)
-#' g.f <- graph(c(1, 2, 1, 3, 1, 4, 2, 5, 2, 6, 2, 7, 3, 8, 3, 9, 3, 10, 4, 11, 4, 12, 4, 13), dir = FALSE)
+#' g.f <- graph(
+#'   c(1, 2, 1, 3, 1, 4, 2, 5, 2, 6, 2, 7, 3, 8, 3, 9, 3, 10, 4, 11, 4, 12, 4, 13),
+#'   dir = FALSE
+#' )
 #' # Compute power centrality scores
 #' for (e in seq(-0.5, .5, by = 0.1)) {
 #'   print(round(power_centrality(g.c, exp = e)[c(1, 2, 4)], 2))
