279a152acae5c9917842069898cf015fab8d9088
Kirill Müller <krlmlr+r@mailbox.org>
2023-01-21 10:54:56 +0100

    Use high-level attribute setters when adding vertices and edges

--- R/interface.R
+++ R/interface.R
@@ -84,12 +84,11 @@ add_edges <- function(graph, edges, ..., attr = list()) {
     idx <- numeric()
   }
 
-  eattrs <- .Call(C_R_igraph_mybracket2, graph, igraph_t_idx_attr, igraph_attr_idx_edge)
   for (i in seq(attrs)) {
-    eattrs[[nam[i]]][idx] <- attrs[[nam[i]]]
+    edge_attr(graph, nam[[i]], idx) <- attrs[[nam[i]]]
   }
 
-  .Call(C_R_igraph_mybracket2_set, graph, igraph_t_idx_attr, igraph_attr_idx_edge, eattrs)
+  graph
 }
 
 #' Add vertices to a graph
@@ -148,12 +147,11 @@ add_vertices <- function(graph, nv, ..., attr = list()) {
     idx <- numeric()
   }
 
-  vattrs <- .Call(C_R_igraph_mybracket2, graph, igraph_t_idx_attr, igraph_attr_idx_vertex)
   for (i in seq(attrs)) {
-    vattrs[[nam[i]]][idx] <- attrs[[nam[i]]]
+    vertex_attr(graph, nam[[i]], idx) <- attrs[[nam[i]]]
   }
 
-  .Call(C_R_igraph_mybracket2_set, graph, igraph_t_idx_attr, igraph_attr_idx_vertex, vattrs)
+  graph
 }
 
 #' Delete edges from a graph
