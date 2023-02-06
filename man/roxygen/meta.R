995c283f8740d958edf8bcb7083a198477061292
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-02-06 10:19:31 +0100

    more of the same

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -2,6 +2,7 @@ list(
   rd_family_title = list(
     adjacency = "Adjacency matrices",
     attributes = "Vertex, edge and graph attributes",
-    bipartite = "Bipartite graphs"
+    bipartite = "Bipartite graphs",
+    centrality = "Centrality measures"
     )
 )
