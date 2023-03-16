7cecf44e139a6939d52bf0cbf2d69ed24f4ef88f
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-03-16 11:36:47 +0100

    find all deprecated calls

---
+++ inst/deprecate.R
@@ -0,0 +1,27 @@
+script <- here::here("R", "zzz-deprecate.R")
+p <- parse(
+  file = script,
+  keep.source = TRUE
+  )
+
+xml <- p |>
+  xmlparsedata::xml_parse_data(pretty = TRUE) |>
+  xml2::read_xml()
+
+# extract all calls to deprecated()
+deprecated_calls <- xml2::xml_find_all(
+  xml,
+  ".//SYMBOL_FUNCTION_CALL[text()='deprecated']"
+)
+
+tibblify_call <- function(deprecated_call) {
+  args <- deprecated_call |>
+    xml2::xml_parent() |>
+    xml2::xml_siblings() |>
+    purrr::keep(~xml2::xml_name(.x) == "expr")
+  old <- xml2::xml_text(args[[1]])
+  new <- xml2::xml_text(args[[2]])
+  tibble::tibble(old = gsub('"', '', old), new = new)
+}
+
+deprecated_df <- purrr::map_df(deprecated_calls, tibblify_call)
