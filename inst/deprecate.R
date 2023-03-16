c9a2fe5a71895799302e2055eef147212218875a
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-03-16 12:05:07 +0100

    progress

--- inst/deprecate.R
+++ inst/deprecate.R
@@ -1,12 +1,14 @@
-script <- here::here("R", "zzz-deprecate.R")
-p <- parse(
-  file = script,
-  keep.source = TRUE
-  )
+# parse script ----
+zzz_script <- here::here("R", "zzz-deprecate.R")
 
-xml <- p |>
-  xmlparsedata::xml_parse_data(pretty = TRUE) |>
-  xml2::read_xml()
+parse_script <- function(path) {
+  path |>
+    parse(keep.source = TRUE) |>
+    xmlparsedata::xml_parse_data(pretty = TRUE) |>
+    xml2::read_xml()
+}
+
+xml <- parse_script(zzz_script)
 
 # extract all calls to deprecated()
 deprecated_calls <- xml2::xml_find_all(
@@ -25,3 +27,36 @@ tibblify_call <- function(deprecated_call) {
 }
 
 deprecated_df <- purrr::map_df(deprecated_calls, tibblify_call)
+
+# parse ALL the package ----
+scripts <- fs::dir_ls(here::here("R")) |>
+  purrr::keep(~(.x != zzz_script)) |>
+  purrr::map(parse_script)
+
+parse_script_function_call <- function(script, script_name) {
+  fns <- xml2::xml_find_all(script, ".//FUNCTION")
+  parse_function <- function(fn) {
+    whole_definition <- fn |> xml2::xml_parent()|> xml2::xml_parent()
+    line1 <- xml2::xml_attr(whole_definition, "line1")
+    line2 <- xml2::xml_attr(whole_definition, "line2")
+    name <- whole_definition |> xml2::xml_child() |> xml2::xml_text()
+    tibble::tibble(
+      line1 = line1,
+      line2 = line2,
+      name = name
+    )
+  }
+  script_df <- purrr::map_df(fns, parse_function)
+  script_df$script_name <- script_name
+  script_df
+}
+
+pkg_defs <- purrr::map2_df(scripts, names(scripts), parse_script_function_call)
+
+# treat calls ----
+treat_call <- function(old, new) {
+  # find where new is defined
+}
+
+# delete script ----
+# fs::file_delete(zzz_script)
