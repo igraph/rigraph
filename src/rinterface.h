dcb21c4cc267c7705bb88c77994e011fd404edbf
antonov548 <antonov5551998@gmail.com>
2023-04-02 13:42:18 +0300

    fix prototypes

--- src/rinterface.h
+++ src/rinterface.h
@@ -31,9 +31,9 @@
 SEXP R_igraph_add_env(SEXP graph);
 
 void R_igraph_set_in_r_check(bool set);
-void R_igraph_error();
-void R_igraph_warning();
-void R_igraph_interrupt();
+void R_igraph_error(void);
+void R_igraph_warning(void);
+void R_igraph_interrupt(void);
 
 #define IGRAPH_R_CHECK(func) \
     do { \
