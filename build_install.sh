14a83f9d3e252ba6d508ba9040ec3e31f256c522
antonov548 <antonov5551998@gmail.com>
2023-04-07 18:39:05 +0200

    add igraph version file

--- build_install.sh
+++ build_install.sh
@@ -1,3 +1,11 @@
+# Error handling
+set -eux pipefail
+
+# Set version of igraph
+if ! [ -f ./src/vendor/cigraph/IGRAPH_VERSION ]; then
+    echo "0.10.0" >> ./src/vendor/cigraph/IGRAPH_VERSION
+fi
+
 # Build igraph with cmake
 cmake -H. -Bbuild -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -DCMAKE_SHARED_LIBRARY_PREFIX_CXX=""
 cmake --build build --target install --config Release
