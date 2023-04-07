3b05f8d463c206fd442038d71eab802551c4c252
Kirill Müller <krlmlr+r@mailbox.org>
2023-04-07 19:21:39 +0200

    Move build root to src

--- build_install.sh
+++ build_install.sh
@@ -6,9 +6,11 @@ if ! [ -f ./src/vendor/cigraph/IGRAPH_VERSION ]; then
     echo "0.10.0" >> ./src/vendor/cigraph/IGRAPH_VERSION
 fi
 
+cd src
+
 # Build igraph with cmake
 cmake -H. -Bbuild -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -DCMAKE_SHARED_LIBRARY_PREFIX_CXX=""
 cmake --build build --target install --config Release
 
-cp -r ./install/include/igraph ./src/include
-cp ./install/lib/libigraph.a ./src
+cp -r ./install/include/igraph ./include
+cp ./install/lib/libigraph.a .
