347c72261aa4d103a019345931b23438bcb764be
antonov548 <antonov5551998@gmail.com>
2023-04-07 14:01:46 +0200

    add integer `CALL`

--- src/rinterface.c
+++ src/rinterface.c
@@ -10869,7 +10869,7 @@ SEXP R_igraph_version() {
                                         /* Convert input */
 
                                         /* Call igraph */
-  IGRAPH_R_CHECK(igraph_version(c_version_string, c_major, c_minor, c_subminor));
+  IGRAPH_R_CHECK(igraph_version(c_version_string, &c_major, &c_minor, &c_subminor));
 
                                         /* Convert output */
   PROTECT(r_result=NEW_LIST(4));
