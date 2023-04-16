b938d3e300ac413518a3903e385f8149bfea51fd
Kirill Müller <krlmlr+r@mailbox.org>
2023-04-16 21:46:52 +0200

    Trim config.h

--- src/config.h
+++ src/config.h
@@ -1,9 +1,6 @@
 /* src/config.h.  Generated from config.h.in by configure.  */
 /* src/config.h.in.  Generated from configure.ac by autoheader.  */
 
-/* Define to 1 if you have the `expm1' function. */
-#define HAVE_EXPM1 1
-
 /* Define to 1 if you have the `finite' function. */
 #define HAVE_FINITE 1
 
@@ -15,12 +12,6 @@
 
 /* Define to 1 if you have the GLPK library */
 #define HAVE_GLPK 1
-
-/* Define to 1 if you have the GMP library */
-#define HAVE_GMP 1
-
-/* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the `isfinite' function. */
 #define HAVE_ISFINITE 1
@@ -43,23 +34,11 @@
 /* Define to 1 if you have the <net/if.h> header file. */
 #define HAVE_NET_IF_H 1
 
-/* Define to 1 if you have the `rint' function. */
-#define HAVE_RINT 1
-
-/* Define to 1 if you have the `rintf' function. */
-#define HAVE_RINTF 1
-
 /* Define to 1 if you have the `round' function. */
 #define HAVE_ROUND 1
 
 /* Define if struct sockaddr contains sa_len */
 /* #undef HAVE_SA_LEN */
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
-
-/* Define to 1 if you have the <stdio.h> header file. */
-#define HAVE_STDIO_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
@@ -73,12 +52,6 @@
 /* Define to 1 if you have the `strdup' function. */
 #define HAVE_STRDUP 1
 
-/* Define to 1 if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
-
-/* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
-
 /* Define to 1 if you have the <sys/file.h> header file. */
 #define HAVE_SYS_FILE_H 1
 
@@ -91,20 +64,11 @@
 /* Define to 1 if you have the <sys/sockio.h> header file. */
 /* #undef HAVE_SYS_SOCKIO_H */
 
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
-
 /* Define to 1 if you have the <sys/time.h> header file. */
 #define HAVE_SYS_TIME_H 1
 
-/* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
-
 /* Define to 1 if you have the <sys/un.h> header file. */
 #define HAVE_SYS_UN_H 1
-
-/* Define to 1 if you have the sys/times.h header */
-#define HAVE_TIMES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
@@ -118,26 +82,4 @@
 /* Define to 1 if you use the vendored mini-GMP library */
 #define INTERNAL_GMP 1
 
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT ""
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME ""
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING ""
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME ""
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION ""
-
-/* Define to 1 if all of the C90 standard headers exist (not just the ones
-   required in a freestanding environment). This macro is provided for
-   backward compatibility; new code need not use it. */
-#define STDC_HEADERS 1
 #define HAVE_ISFINITE 1
