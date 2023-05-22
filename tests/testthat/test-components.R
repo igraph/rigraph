145b14ca75088f200d2f8d8618c9e2f9d146ceb0
ngmaclaren <45513933+ngmaclaren@users.noreply.github.com>
2023-05-22 07:58:23 -0400

    Add tests for largest_component()

--- tests/testthat/test-components.R
+++ tests/testthat/test-components.R
@@ -1,1 +1,40 @@
+test_that("largest component is actually the largest", {
+    g <- make_star(20, "undirected")
+    h <- make_ring(10)
+
+    G <- disjoint_union(g, h)
+
+    expect_true(isomorphic(largest_component(G), g))
+})
+
+test_that("largest strongly and weakly components are correct", {
+    g <- graph_from_literal(
+        A -+ B,
+        B -+ C,
+        C -+ A,
+        C -+ D,
+        E
+    )
+
+    strongly <- graph_from_literal(
+        A -+ B,
+        B -+ C,
+        C -+ A
+    )
+    weakly <- graph_from_literal(
+        A -+ B,
+        B -+ C,
+        C -+ A,
+        C -+ D
+    )
+
+    expect_true(isomorphic(largest_component(g, "weak"), weakly))
+    expect_true(isomorphic(largest_component(g, "strong"), strongly))
+})
+
+test_that("the largest component of a null graph is a valid null graph", {
+    nullgraph <- make_empty_graph(0)
+
+    expect_true(isomorphic(largest_component(make_empty_graph(0)), nullgraph))
+})
 
