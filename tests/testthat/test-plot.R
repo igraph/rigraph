6a93ba9ef0fe60a5b6951e6c4674f1e3a6439edb
Kirill Müller <krlmlr+r@mailbox.org>
2023-05-27 14:10:29 +0200

    Un-skip

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -34,10 +34,6 @@ test_that("basic plot test, R >= 4.3", {
 
   skip_if(getRversion() < 4.3)
 
-  # skip unconditionally; I don't have time to figure this out now but
-  # we need the tests to pass
-  skip_if(TRUE)
-
   vdiffr::expect_doppelganger(
     "Basic graph, R >= 4.3",
     function() {
