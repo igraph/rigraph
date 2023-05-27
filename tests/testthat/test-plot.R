b167fbb04607e05a3d58cf4105f7d7cb649711b9
Kirill Müller <krlmlr+r@mailbox.org>
2023-05-27 16:16:21 +0200

    Set seed in worker function

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,11 +1,10 @@
 test_that("basic plot test, R < 4.1", {
-  set.seed("20221214")
-
   skip_if(getRversion() >= 4.1)
 
   vdiffr::expect_doppelganger(
     "Basic graph, R < 4.1",
     function() {
+      set.seed("20221214")
       g <- make_graph(c(1, 2, 1, 1, 2, 3), directed = F)
       cols <- c("red", "green", "blue")
       plot(g, edge.color = cols)
@@ -14,14 +13,13 @@ test_that("basic plot test, R < 4.1", {
 })
 
 test_that("basic plot test, 4.1 <= R < 4.3", {
-  set.seed("20221214")
-
   skip_if(getRversion() < 4.1)
   skip_if(getRversion() >= 4.3)
 
   vdiffr::expect_doppelganger(
     "Basic graph, R < 4.3",
     function() {
+      set.seed("20221214")
       g <- make_graph(c(1, 2, 1, 1, 2, 3), directed = F)
       cols <- c("red", "green", "blue")
       plot(g, edge.color = cols)
@@ -30,13 +28,12 @@ test_that("basic plot test, 4.1 <= R < 4.3", {
 })
 
 test_that("basic plot test, R >= 4.3", {
-  set.seed("20221214")
-
   skip_if(getRversion() < 4.3)
 
   vdiffr::expect_doppelganger(
     "Basic graph, R 4.3 and later",
     function() {
+      set.seed("20221214")
       g <- make_graph(c(1, 2, 1, 1, 2, 3), directed = F)
       cols <- c("red", "green", "blue")
       plot(g, edge.color = cols)
