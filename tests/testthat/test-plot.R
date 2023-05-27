404ad3021cd349d1af3c6bc1c0c131f98aea1af0
Kirill Müller <krlmlr+r@mailbox.org>
2023-05-27 16:12:46 +0200

    Fix name clash

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -35,7 +35,7 @@ test_that("basic plot test, R >= 4.3", {
   skip_if(getRversion() < 4.3)
 
   vdiffr::expect_doppelganger(
-    "Basic graph, R >= 4.3",
+    "Basic graph, R 4.3 and later",
     function() {
       g <- make_graph(c(1, 2, 1, 1, 2, 3), directed = F)
       cols <- c("red", "green", "blue")
