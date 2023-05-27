a5fdf9c6a5c119828712bf03fa29d981cdd18796
Kirill Müller <krlmlr+r@mailbox.org>
2023-05-27 14:10:20 +0200

    Split tests

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,10 +1,26 @@
-test_that("basic plot test, R < 4.3", {
+test_that("basic plot test, R < 4.1", {
   set.seed("20221214")
 
+  skip_if(getRversion() >= 4.1)
+
+  vdiffr::expect_doppelganger(
+    "Basic graph, R < 4.1",
+    function() {
+      g <- make_graph(c(1, 2, 1, 1, 2, 3), directed = F)
+      cols <- c("red", "green", "blue")
+      plot(g, edge.color = cols)
+    }
+  )
+})
+
+test_that("basic plot test, 4.1 <= R < 4.3", {
+  set.seed("20221214")
+
+  skip_if(getRversion() < 4.1)
   skip_if(getRversion() >= 4.3)
 
   vdiffr::expect_doppelganger(
-    "Basic graph, R <= 4.2",
+    "Basic graph, R < 4.3",
     function() {
       g <- make_graph(c(1, 2, 1, 1, 2, 3), directed = F)
       cols <- c("red", "green", "blue")
