275fef78cba955f472b22a9636e00e6447eae580
Antonov548 <antonov5551998@gmail.com>
2023-06-28 12:53:50 +0200

    fix `R_SEXP_to_igraph_es`

--- src/rinterface_extra.c
+++ src/rinterface_extra.c
@@ -3810,9 +3810,9 @@ int R_SEXP_to_igraph_vs(SEXP rit, igraph_t *graph, igraph_vs_t *it) {
 
 int R_SEXP_to_igraph_es(SEXP rit, igraph_t *graph, igraph_es_t *it) {
 
-  igraph_vector_t *tmpv=(igraph_vector_t*)R_alloc(1,sizeof(igraph_vector_t));
-  igraph_es_vector(it, igraph_vector_view(tmpv, REAL(rit),
-                                          GET_LENGTH(rit)));
+  igraph_vector_int_t *tmpv=(igraph_vector_int_t*)R_alloc(1,sizeof(igraph_vector_int_t));
+  igraph_es_vector(it, igraph_vector_int_view(tmpv, INTEGER(rit), LENGTH(rit)));
+
   return 0;
 }
 
