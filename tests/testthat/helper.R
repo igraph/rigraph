b95912aa704f6e9b56112ca24af048668789b0f4
Kirill Müller <kirill@cynkra.com>
2023-08-09 05:43:09 +0200

    chore: Add cran_install() helper

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -25,3 +25,9 @@ expect_that <- function(object, condition, info = NULL, label = NULL) {
     condition(object)
   )
 }
+
+cran_install <- function() {
+  unlink("cran", recursive = TRUE)
+  dir.create("cran")
+  pak::pak("igraph", lib = "cran", dependencies = FALSE)
+}
