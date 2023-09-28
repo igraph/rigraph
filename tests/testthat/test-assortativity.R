48a7e69fdfac4beb44193d7d06efbffc0f61a0e4
Kirill Müller <kirill@cynkra.com>
2023-09-28 06:56:58 -0700

    1

--- tests/testthat/test-assortativity.R
+++ tests/testthat/test-assortativity.R
@@ -1,17 +1,4 @@
 test_that("assortativity works", {
   writeLines(character(), file.path(Sys.getenv("RUNNER_TEMP"), "1.txt"))
   g <- read_graph(f <- gzfile("celegansneural.gml.gz"), format = "gml")
-
-  writeLines(character(), file.path(Sys.getenv("RUNNER_TEMP"), "2.txt"))
-  assR <- function(graph) {
-    indeg <- degree(graph, mode = "in")
-    outdeg <- degree(graph, mode = "out")
-    el <- as_edgelist(graph, names = FALSE)
-    J <- outdeg[el[, 1]] - 1
-    K <- indeg[el[, 2]] - 1
-    num <- sum(J * K) - sum(J) * sum(K) / ecount(graph)
-    den1 <- sum(J * J) - sum(J)^2 / ecount(graph)
-    den2 <- sum(K * K) - sum(K)^2 / ecount(graph)
-    num / sqrt(den1) / sqrt(den2)
-  }
 })
