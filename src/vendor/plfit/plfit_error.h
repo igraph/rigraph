990919c17dd5992d25887a4a9b1e653f802a3b6b
Kirill Müller <kirill@cynkra.com>
2023-09-28 06:46:32 -0700

    chore: Restore vendored plfit, remove from vendored cigraph for now

--- src/vendor/plfit/plfit_error.h
+++ src/vendor/plfit/plfit_error.h
@@ -38,8 +38,7 @@ enum {
 	PLFIT_EINVAL   = 2,
 	PLFIT_UNDRFLOW = 3,
 	PLFIT_OVERFLOW = 4,
-	PLFIT_ENOMEM   = 5,
-	PLFIT_EMAXITER = 6
+	PLFIT_ENOMEM   = 5
 };
 
 #if (defined(__GNUC__) && GCC_VERSION_MAJOR >= 3)
@@ -56,7 +55,7 @@ enum {
 		if (PLFIT_UNLIKELY(plfit_i_ret != PLFIT_SUCCESS)) {\
 			return plfit_i_ret; \
 		} \
-	} while (0)
+	} while(0)
 
 #define PLFIT_ERROR(reason,plfit_errno) \
 	do {\
