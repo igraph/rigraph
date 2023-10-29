f2c6159cf14a2f35291a6d35512c8e72c5c533b9
Kirill Müller <kirill@cynkra.com>
2023-10-29 07:02:53 +0100

    Stabilize plot test

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,31 +1,43 @@
-test_that("basic plot test, R < 4.3", {
-  set.seed("20221214")
-
-  skip_if(getRversion() >= "4.3")
-  # FIXME: Reenable after separating layouting from plotting
-  skip_on_ci()
+test_that("basic plot test, layout 1", {
+  g <- make_graph(c(1, 2, 1, 1, 2, 3), directed = F)
+  g$layout <- structure(
+    c(
+      1.3334764568601,
+      0.25005693449542,
+      -0.832839300886122,
+      -1.08278419159866,
+      -0.70646229874506,
+      -0.33032215991384
+    ),
+    dim = 3:2
+  )
 
   vdiffr::expect_doppelganger(
-    "Basic graph, R <= 4.2",
+    "Basic graph, layout 1",
     function() {
-      g <- make_graph(c(1, 2, 1, 1, 2, 3), directed = F)
       cols <- c("red", "green", "blue")
       plot(g, edge.color = cols)
     }
   )
 })
 
-test_that("basic plot test, R >= 4.3", {
-  set.seed("20221214")
-
-  skip_if(getRversion() < "4.3")
-  # FIXME: Reenable after separating layouting from plotting
-  skip_on_ci()
+test_that("basic plot test, layout 2", {
+  g <- make_graph(c(1, 2, 1, 1, 2, 3), directed = F)
+  g$layout <- structure(
+    c(
+      0.777280713471641,
+      -0.184447163786926,
+      -1.14684336319031,
+      0.420797824153838,
+      1.04644848108279,
+      1.6725339471037
+    ),
+    dim = 3:2
+  )
 
   vdiffr::expect_doppelganger(
-    "Basic graph, R >= 4.3",
+    "Basic graph, layout 2",
     function() {
-      g <- make_graph(c(1, 2, 1, 1, 2, 3), directed = F)
       cols <- c("red", "green", "blue")
       plot(g, edge.color = cols)
     }
