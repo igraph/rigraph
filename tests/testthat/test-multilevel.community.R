f74002deea9714a36aebb2753e710f8b15f9ecee
Kirill Müller <kirill@cynkra.com>
2023-10-30 05:24:01 +0100

    test: Make `cluster_louvain()` test bullet-proof

--- tests/testthat/test-multilevel.community.R
+++ tests/testthat/test-multilevel.community.R
@@ -8,7 +8,7 @@ test_that("cluster_louvain works", {
   expect_that(modularity(g, mc$membership), equals(max(mc$modularity)))
   # 3   4
   # 2 998
-  expect_that(length(mc), equals(4))
+  expect_true(length(mc) %in% 3:4)
   expect_true(all(as.vector(membership(mc)) %in% seq_len(length(mc))))
   expect_s3_class(sizes(mc), "table")
   expect_equal(sum(sizes(mc)), vcount(g))
