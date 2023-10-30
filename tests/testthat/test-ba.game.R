182e0a39d13826c0d98c33f440534b3ea1cb9cb2
Kirill Müller <kirill@cynkra.com>
2023-10-30 05:25:24 +0100

    Include low-probability outcomes

--- tests/testthat/test-ba.game.R
+++ tests/testthat/test-ba.game.R
@@ -23,13 +23,13 @@ test_that("sample_pa can start from a graph", {
   expect_that(vcount(g4), equals(10))
   #  0    1    2    3    4
   # 24  809 3904 4240 1023
-  expect_true(sum(degree(g4) == 0) %in% 1:4)
+  expect_true(sum(degree(g4) == 0) %in% 0:4)
   #  2    3    4    5    6    7    8   10
   # 25  302 1820 2563 3350 1093  816   31
-  expect_true(sum(degree(g4) == 1) %in% 3:8)
+  expect_true(sum(degree(g4) == 1) %in% c(2:8, 10L))
   #   0    1    2    3    4
   # 879 2271 5289 1532   29
-  expect_true(sum(degree(g4) %in% 2:3) %in% 0:3)
+  expect_true(sum(degree(g4) %in% 2:3) %in% 0:4)
 
   g6 <- sample_pa(10, m = 1, algorithm = "bag", start.graph = make_star(10))
   expect_true(graph.isomorphic(g6, make_star(10)))
