0845bfb29407ad6a84442b15027d49817625b4b4
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-11-13 10:25:07 +0100

    rm useless alias

--- R/centrality.R
+++ R/centrality.R
@@ -222,7 +222,7 @@ edge.betweenness.estimate <- estimate_edge_betweenness
 #' graphs, consider using the harmonic centrality with
 #' [harmonic_centrality()]
 #'
-#' @aliases closeness closeness.estimate
+#' @aliases closeness.estimate
 #' @param graph The graph to analyze.
 #' @param vids The vertices for which closeness will be calculated.
 #' @param mode Character string, defined the types of the paths used for
