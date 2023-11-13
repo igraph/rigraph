a2cd8a390e0c46b0d1e26cbcfdbfc91fdace0558
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-11-13 09:55:49 +0100

    rm more aliases

--- R/cliques.R
+++ R/cliques.R
@@ -44,8 +44,7 @@
 #' `clique_size_counts()` returns a numeric vector representing a histogram
 #' of clique sizes, between the given minimum and maximum clique size.
 #'
-#' @aliases cliques largest_cliques maximal.cliques maximal.cliques.count
-#' clique.number largest.cliques
+#' @aliases maximal.cliques maximal.cliques.count clique.number largest.cliques
 #' @param graph The input graph, directed graphs will be considered as
 #'   undirected ones, multiple edges and loops are ignored.
 #' @param min Numeric constant, lower limit on the size of the cliques to find.
