c685c2554ad4624e88cf65d7d9af7218abd1f45b
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-12-01 15:48:03 +0100

    fix indentation

--- R/make.R
+++ R/make.R
@@ -656,15 +656,15 @@ make_graph <- function(edges, ..., n = max(edges), isolates = NULL,
 graph <- function(edges , ... , n = max(edges) , isolates = NULL , directed = TRUE , dir = directed , simplify = TRUE) { # nocov start
    lifecycle::deprecate_soft("1.6.0", "graph()", "make_graph()")
 
-   if (!missing(dir) && missing(directed)) {
-     directed <- dir
-   }
+  if (!missing(dir) && missing(directed)) {
+    directed <- dir
+  }
 
-   if (missing(simplify)) {
-   make_graph(edges = edges, n = n, isolates = isolates, directed = directed, ...)
-   } else {
-   make_graph(edges = edges, n = n, isolates = isolates, directed = directed, simplify = simplify, ...)
-   }
+  if (missing(simplify)) {
+    make_graph(edges = edges, n = n, isolates = isolates, directed = directed, ...)
+  } else {
+    make_graph(edges = edges, n = n, isolates = isolates, directed = directed, simplify = simplify, ...)
+  }
 } # nocov end
 
 #' Create an igraph graph from a list of edges, or a notable graph
