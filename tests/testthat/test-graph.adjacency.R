3769c78c8e10eb1758b6f0c526ff48f4e8b7c25d
Kirill Müller <kirill@cynkra.com>
2023-12-09 12:32:17 +0100

    Duplicate tests for sparse matrices

--- tests/testthat/test-graph.adjacency.R
+++ tests/testthat/test-graph.adjacency.R
@@ -231,6 +231,241 @@ test_that("graph_from_adjacency_matrix works", {
   )
 })
 
+test_that("graph_from_adjacency_matrix works", {
+  skip_if_not_installed("Matrix")
+
+  M1 <- rbind(
+    c(0, 0, 1, 1),
+    c(1, 0, 0, 0),
+    c(0, 1, 0, 1),
+    c(1, 0, 0, 1)
+  )
+  g1 <- graph_from_adjacency_matrix(as(M1, "dgCMatrix"))
+  el1 <- as_edgelist(g1)
+  expect_equal(
+    el1[order(el1[, 1], el1[, 2]), ],
+    cbind(
+      c(1, 1, 2, 3, 3, 4, 4),
+      c(3, 4, 1, 2, 4, 1, 4)
+    )
+  )
+
+  M2 <- rbind(
+    c(0, 1, 1, 1),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 1),
+    c(1, 0, 1, 0)
+  )
+  g2 <- graph_from_adjacency_matrix(as(M2, "dgCMatrix"), mode = "undirected")
+  el2 <- as_edgelist(g2)
+  expect_equal(
+    el2[order(el2[, 1], el2[, 2]), ],
+    cbind(
+      c(1, 1, 1, 3),
+      c(2, 3, 4, 4)
+    )
+  )
+
+  M3 <- rbind(
+    c(0, 1, 1, 2),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 0),
+    c(1, 0, 1, 0)
+  )
+  g3 <- graph_from_adjacency_matrix(as(M3, "dgCMatrix"), mode = "min")
+  el3 <- as_edgelist(g3)
+  expect_equal(
+    el3[order(el3[, 1], el3[, 2]), ],
+    cbind(
+      c(1, 1, 1),
+      c(2, 3, 4)
+    )
+  )
+
+  M4 <- rbind(
+    c(0, 1, 1, 2),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 0),
+    c(1, 0, 1, 0)
+  )
+  g4 <- graph_from_adjacency_matrix(as(M4, "dgCMatrix"), mode = "max")
+  el4 <- as_edgelist(g4)
+  expect_equal(
+    el4[order(el4[, 1], el4[, 2]), ],
+    cbind(
+      c(1, 1, 1, 1, 3),
+      c(2, 3, 4, 4, 4)
+    )
+  )
+
+  M5 <- rbind(
+    c(0, 1, 1, 2),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 0),
+    c(1, 0, 1, 0)
+  )
+  g5 <- graph_from_adjacency_matrix(as(M5, "dgCMatrix"), mode = "upper")
+  el5 <- as_edgelist(g5)
+  expect_equal(
+    el5[order(el5[, 1], el5[, 2]), ],
+    cbind(
+      c(1, 1, 1, 1),
+      c(2, 3, 4, 4)
+    )
+  )
+
+  M6 <- rbind(
+    c(0, 1, 1, 2),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 0),
+    c(1, 0, 1, 0)
+  )
+  g6 <- graph_from_adjacency_matrix(as(M6, "dgCMatrix"), mode = "lower")
+  el6 <- as_edgelist(g6)
+  expect_equal(
+    el6[order(el6[, 1], el6[, 2]), ],
+    cbind(
+      c(1, 1, 1, 3),
+      c(2, 3, 4, 4)
+    )
+  )
+
+  M7 <- rbind(
+    c(0, 1, 1, 2),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 0),
+    c(1, 0, 1, 0)
+  )
+  g7 <- graph_from_adjacency_matrix(as(M7, "dgCMatrix"), mode = "plus")
+  el7 <- as_edgelist(g7)
+  expect_equal(
+    el7[order(el7[, 1], el7[, 2]), ],
+    cbind(
+      c(1, 1, 1, 1, 1, 1, 1, 3),
+      c(2, 2, 3, 3, 4, 4, 4, 4)
+    )
+  )
+
+  M8 <- rbind(
+    c(0, 1, 1, 0.5),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 0),
+    c(1, 0, 2, 0)
+  )
+  g8 <- graph_from_adjacency_matrix(as(M8, "dgCMatrix"), mode = "directed", weighted = TRUE)
+  el8 <- cbind(as_edgelist(g8), E(g8)$weight)
+  expect_equal(
+    el8[order(el8[, 1], el8[, 2]), ],
+    cbind(
+      c(1, 1, 1, 2, 3, 4, 4),
+      c(2, 3, 4, 1, 1, 1, 3),
+      c(1, 1, 0.5, 1, 1, 1, 2)
+    )
+  )
+
+  M9 <- rbind(
+    c(0, 1, 1, 3),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 2),
+    c(3, 0, 2, 0)
+  )
+  g9 <- graph_from_adjacency_matrix(as(M9, "dgCMatrix"), mode = "undirected", weighted = TRUE)
+  el9 <- cbind(as_edgelist(g9), E(g9)$weight)
+  expect_equal(
+    el9[order(el9[, 1], el9[, 2]), ],
+    cbind(
+      c(1, 1, 1, 3),
+      c(2, 3, 4, 4),
+      c(1, 1, 3, 2)
+    )
+  )
+
+  M10 <- rbind(
+    c(0, 1, 1, 0.5),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 0),
+    c(1, 0, 2, 0)
+  )
+  g10 <- graph_from_adjacency_matrix(as(M10, "dgCMatrix"), mode = "max", weighted = TRUE)
+  el10 <- cbind(as_edgelist(g10), E(g10)$weight)
+  expect_equal(
+    el10[order(el10[, 1], el10[, 2]), ],
+    cbind(
+      c(1, 1, 1, 3),
+      c(2, 3, 4, 4),
+      c(1, 1, 1, 2)
+    )
+  )
+
+  M11 <- rbind(
+    c(0, 1, 1, 0.5),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 0),
+    c(1, 0, 2, 0)
+  )
+  g11 <- graph_from_adjacency_matrix(as(M11, "dgCMatrix"), mode = "min", weighted = TRUE)
+  el11 <- cbind(as_edgelist(g11), E(g11)$weight)
+  expect_equal(
+    el11[order(el11[, 1], el11[, 2]), ],
+    cbind(
+      c(1, 1, 1),
+      c(2, 3, 4),
+      c(1, 1, 0.5)
+    )
+  )
+
+  M12 <- rbind(
+    c(0, 1, 1, 0.5),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 0),
+    c(1, 0, 2, 0)
+  )
+  g12 <- graph_from_adjacency_matrix(as(M12, "dgCMatrix"), mode = "lower", weighted = TRUE)
+  el12 <- cbind(as_edgelist(g12), E(g12)$weight)
+  expect_equal(
+    el12[order(el12[, 1], el12[, 2]), ],
+    cbind(
+      c(1, 1, 1, 3),
+      c(2, 3, 4, 4),
+      c(1, 1, 1, 2)
+    )
+  )
+
+  M13 <- rbind(
+    c(0, 1, 1, 0.5),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 0),
+    c(1, 0, 2, 0)
+  )
+  g13 <- graph_from_adjacency_matrix(as(M13, "dgCMatrix"), mode = "upper", weighted = TRUE)
+  el13 <- cbind(as_edgelist(g13), E(g13)$weight)
+  expect_equal(
+    el13[order(el13[, 1], el13[, 2]), ],
+    cbind(
+      c(1, 1, 1),
+      c(2, 3, 4),
+      c(1, 1, 0.5)
+    )
+  )
+
+  M14 <- rbind(
+    c(0, 1, 1, 0.5),
+    c(1, 0, 0, 0),
+    c(1, 0, 0, 0),
+    c(1, 0, 2, 0)
+  )
+  g14 <- graph_from_adjacency_matrix(as(M14, "dgCMatrix"), mode = "plus", weighted = TRUE)
+  el14 <- cbind(as_edgelist(g14), E(g14)$weight)
+  expect_equal(
+    el14[order(el14[, 1], el14[, 2]), ],
+    cbind(
+      c(1, 1, 1, 3),
+      c(2, 3, 4, 4),
+      c(2, 2, 1.5, 2)
+    )
+  )
+})
+
 test_that("graph_from_adjacency_matrix() snapshot", {
   rlang::local_options(lifecycle_verbosity = "warning")
 
