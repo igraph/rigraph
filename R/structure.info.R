88454e9efbc48ec64db934ab020a2cba35ed927c
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-12-11 14:34:00 +0100

    refactor!: change R/structure.info.R

--- R/structure.info.R
+++ R/structure.info.R
@@ -1,4 +1,19 @@
 
+#' Are two vertices adjacent?
+#'
+#' @description
+#' `r lifecycle::badge("deprecated")`
+#'
+#' `are.connected()` was renamed to `are_adjacent()` to create a more
+#' consistent API.
+#' @inheritParams are_adjacent
+#' @keywords internal
+#' @export
+are.connected <- function(graph, v1, v2) { # nocov start
+  lifecycle::deprecate_soft("1.7.0", "are.connected()", "are_adjacent()")
+  are_adjacent(graph = graph, v1 = v1, v2 = v2)
+} # nocov end
+
 #   IGraph R package
 #   Copyright (C) 2005-2012  Gabor Csardi <csardi.gabor@gmail.com>
 #   334 Harvard street, Cambridge, MA 02139 USA
@@ -25,7 +40,6 @@
 #' The order of the vertices only matters in directed graphs,
 #' where the existence of a directed `(v1, v2)` edge is queried.
 #'
-#' @aliases are.connected
 #' @param graph The graph.
 #' @param v1 The first vertex, tail in directed graphs.
 #' @param v2 The second vertex, head in directed graphs.
@@ -55,4 +69,3 @@ are_adjacent <- function(graph, v1, v2) {
   )
 }
 #' @export are.connected
-deprecated("are.connected", are_adjacent)
