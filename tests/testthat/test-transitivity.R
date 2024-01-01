ba73fc74fccb7ec082f83649834b7283b5abb6c0
Szabolcs Horvát <szhorvat@gmail.com>
2024-01-01 13:07:18 +0000

    tests: check that transitivity() produces named vectors

--- tests/testthat/test-transitivity.R
+++ tests/testthat/test-transitivity.R
@@ -27,3 +27,21 @@ test_that("no integer overflow", {
   mtr <- min(transitivity(g, type = "local"), na.rm = TRUE)
   expect_true(mtr > 0)
 })
+
+# Check that transitivity() produces named vectors, see #943
+# The four tests below check four existing code paths
+test_that("local transitivity produces named vectors", {
+  g <- make_graph(~ a-b-c-a-d)
+  E(g)$weight <- 1:4
+  t1 <- transitivity(g, type = "local")
+  t2 <- transitivity(g, type = "barrat")
+
+  vs <- c("a", "c")
+  t3 <- transitivity(g, type = "local", vids = vs)
+  t4 <- transitivity(g, type = "barrat", vids = vs)
+
+  expect_equal(names(t1), V(g)$name)
+  expect_equal(names(t2), V(g)$name)
+  expect_equal(names(t3), vs)
+  expect_equal(names(t4), vs)
+})
