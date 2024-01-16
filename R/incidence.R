aee342c74476d62a8177d4585fb8541c7431f7dd
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-01-16 10:16:29 +0100

    oops

--- R/incidence.R
+++ R/incidence.R
@@ -277,7 +277,7 @@ graph_from_biadjacency_matrix <- function(incidence, directed = FALSE,
 #' this naming to avoid confusion with the edge-vertex incidence matrix.
 #' @export
 from_incidence_matrix <- function(...) { # nocov start
-   lifecycle::deprecate_warn("20.0", "graph_from_incidence_matrix()", "graph_from_biadjacency_matrix()")
+   lifecycle::deprecate_warn("2.0.0", "graph_from_incidence_matrix()", "graph_from_biadjacency_matrix()")
    graph_from_biadjacency_matrix(...)
 } # nocov end
 #' From incidence matrix
