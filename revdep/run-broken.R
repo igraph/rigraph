95cb300a903cec6197e6b336607dff18269dc07c
Kirill Müller <kirill@cynkra.com>
2024-01-21 06:36:13 +0100

    chore: Tweak revdepcheck script

--- revdep/run-broken.R
+++ revdep/run-broken.R
@@ -1,7 +1,9 @@
-broken <- revdepcheck::cloud_broken()
+problems <- gsub("^# ", "", grep("^# ", readLines("revdep/problems.md"), value = TRUE))
+# broken <- revdepcheck::cloud_broken(install_failures = TRUE, timeout_failures = TRUE)
 failed <- revdepcheck::cloud_failed()
+failures <- gsub("^# ", "", grep("^# ", readLines("revdep/failures.md"), value = TRUE))
 
-packages <- c(broken, failed)
+packages <- c(problems, failed, failures)
 revdepcheck::cloud_check(revdep_packages = packages)
 revdepcheck::cloud_status()
 revdepcheck::cloud_report()
