5b8e2dc5b321b8855c628b0038f42a3a22b46fac
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-01-23 13:52:54 +0100

    :nail_care:

--- tests/testthat/test-get.incidence.R
+++ tests/testthat/test-get.incidence.R
@@ -1,4 +1,4 @@
-test_that("as_biadjacency_matrix works -- dense", {
+test_that("as_biadjacency_matrix() works -- dense", {
   I <- matrix(sample(0:1, 35, replace = TRUE, prob = c(3, 1)), ncol = 5)
   g <- graph_from_biadjacency_matrix(I)
   I2 <- as_biadjacency_matrix(g)
@@ -7,7 +7,7 @@ test_that("as_biadjacency_matrix works -- dense", {
   expect_that(colnames(I2), equals(as.character(8:12)))
 })
 
-test_that("as_biadjacency_matrix works -- sparse", {
+test_that("as_biadjacency_matrix() works -- sparse", {
   I <- matrix(sample(0:1, 35, replace = TRUE, prob = c(3, 1)), ncol = 5)
   g <- graph_from_biadjacency_matrix(I)
   I3 <- as_biadjacency_matrix(g, sparse = TRUE)
