b9fcd842171f0856f154d3b017fb08862be2c787
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-01-29 09:12:44 +0100

    refactor: use `rlang::arg_match()` in `igraph.match.arg()`

--- R/utils-assert-args.R
+++ R/utils-assert-args.R
@@ -16,3 +16,20 @@ ensure_igraph <- function(graph, optional = FALSE) {
     cli::cli_abort("Must provide a graph object (provided wrong object type).")
   }
 }
+
+
+igraph.match.arg <- function(arg, values, error_call = rlang::caller_env()) {
+  if (missing(values)) {
+    formal.args <- formals(sys.function(sys.parent()))
+    values <- eval(formal.args[[deparse(substitute(arg))]])
+  }
+
+  arg <- tolower(arg)
+  values <- tolower(values)
+
+  rlang::arg_match(
+    arg = arg,
+    values = values,
+    error_call = error_call
+  )
+}
