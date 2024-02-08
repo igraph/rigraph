96c2074dc94b5a52746fb70779e60c42bff6b3c8
Kirill Müller <kirill@cynkra.com>
2024-02-08 20:29:29 +0100

    Test numeric case

--- tests/testthat/test-biconnected.components.R
+++ tests/testthat/test-biconnected.components.R
@@ -29,6 +29,35 @@ test_that("biconnected_components works", {
   expect_s3_class(bc$component_edges[[1]], "igraph.es")
 })
 
+test_that("biconnected_components works without igraph.vs.es", {
+  local_igraph_options(return.vs.es = FALSE)
+
+  g <- make_full_graph(5) + make_full_graph(5)
+  clu <- components(g)$membership
+  g <- add_edges(g, c(match(1, clu), match(2, clu)))
+
+  sortlist <- function(list) {
+    list <- lapply(list, sort)
+    list[order(sapply(list, paste, collapse = "x"))]
+  }
+
+  bc <- biconnected_components(g)
+  expect_that(bc$no, equals(3))
+  expect_that(sortlist(bc$tree_edges), equals(list(
+    c(11, 15, 18, 20),
+    c(1, 5, 8, 10), 21
+  )))
+  expect_that(sortlist(bc$component_edges), equals(list(11:20, 1:10, 21)))
+  expect_that(sortlist(bc$components), equals(list(1:5, c(1, 6), 6:10)))
+  expect_that(sort(bc$articulation_points), equals(c(1, 6)))
+
+  expect_equal(sort(names(bc)), c("articulation_points",
+                                  "component_edges",
+                                  "components",
+                                  "no",
+                                  "tree_edges"))
+})
+
 test_that("is_biconnected works", {
   g <- make_full_graph(0)
   expect_false(is_biconnected(g))
