a2a86d24ab2b0f25409997e9e08aec77fed8afa2
Antonov548 <antonov5551998@gmail.com>
2024-02-21 13:19:43 +0100

    update update-cigraph script to extract version

--- tools/update-cigraph.sh
+++ tools/update-cigraph.sh
@@ -6,16 +6,18 @@ rm -rf src/vendor/cigraph
 
 mkdir -p src/vendor
 
-git clone https://github.com/igraph/igraph --depth=1 src/vendor/cigraph
+git clone https://github.com/igraph/igraph src/vendor/cigraph
 
 if [ -n "${1-}" ]; then
   git -C src/vendor/cigraph switch --detach "$1"
 fi
+
+cmake -Ssrc/vendor/cigraph -Bsrc/vendor/cigraph/build
 
 version=$(git -C src/vendor/cigraph rev-parse HEAD)
 
 rm -rf src/vendor/cigraph/.git
 
 rm -rf src/vendor/cigraph/.github src/vendor/cigraph/doc src/vendor/cigraph/examples src/vendor/cigraph/fuzzing src/vendor/cigraph/tests src/vendor/cigraph/tools
-git add src/vendor/cigraph
-git commit -m "chore: Update vendored igraph/C to igraph/igraph@${version}"
+#git add src/vendor/cigraph
+#git commit -m "chore: Update vendored igraph/C to igraph/igraph@${version}"
