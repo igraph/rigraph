494e8800746ec6a356d6ab3e5cea12b4e61b5ed1
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-03-12 13:47:29 +0100

    docs: fix graph_from_adjacency_matrix() examples to avoid warnings, and for clarity. Thanks to @clpippel's help!

--- R/adjacency.R
+++ R/adjacency.R
@@ -344,53 +344,107 @@ graph.adjacency.sparse <- function(adjmatrix, mode, weighted = NULL, diag = TRUE
 #' @keywords graphs
 #' @examples
 #'
-#' adjm <- matrix(sample(0:1, 100, replace = TRUE, prob = c(0.9, 0.1)), ncol = 10)
-#' g1 <- graph_from_adjacency_matrix(adjm)
-#' adjm <- matrix(sample(0:5, 100,
-#'   replace = TRUE,
-#'   prob = c(0.9, 0.02, 0.02, 0.02, 0.02, 0.02)
-#' ), ncol = 10)
-#' g2 <- graph_from_adjacency_matrix(adjm, weighted = TRUE)
+#' g1 <- sample(
+#'     x = 0:1, size = 100, replace = TRUE,
+#'     prob = c(0.9, 0.1)
+#'   ) %>%
+#'   matrix(ncol = 10) %>%
+#'   graph_from_adjacency_matrix()
+#'
+#' g2 <- sample(
+#'     x = 0:5, size = 100, replace = TRUE,
+#'     prob = c(0.9, 0.02, 0.02, 0.02, 0.02, 0.02)
+#' ) %>%
+#'   matrix(ncol = 10) %>%
+#'   graph_from_adjacency_matrix(weighted = TRUE)
 #' E(g2)$weight
 #'
 #' ## various modes for weighted graphs, with some tests
-#' nzs <- function(x) sort(x[x != 0])
-#' adjm <- matrix(runif(100), 10)
-#' adjm[adjm < 0.5] <- 0
-#' g3 <- graph_from_adjacency_matrix((adjm + t(adjm)) / 2,
+#' non_zero_sort <- function(x) sort(x[x != 0])
+#' adj_matrix <- matrix(runif(100), 10)
+#' adj_matrix[adj_matrix < 0.5] <- 0
+#' g3 <- graph_from_adjacency_matrix(
+#'   (adj_matrix + t(adj_matrix)) / 2,
 #'   weighted = TRUE,
 #'   mode = "undirected"
 #' )
 #'
-#' g4 <- graph_from_adjacency_matrix(adjm, weighted = TRUE, mode = "max")
-#' all(nzs(pmax(adjm, t(adjm))[upper.tri(adjm)]) == sort(E(g4)$weight))
+#' g4 <- graph_from_adjacency_matrix(
+#'   adj_matrix,
+#'   weighted = TRUE,
+#'   mode = "max"
+#' )
+#' expected_g4_weights <- non_zero_sort(
+#'   pmax(adj_matrix, t(adj_matrix))[upper.tri(adj_matrix, diag = TRUE)]
+#' )
+#' actual_g4_weights <- sort(E(g4)$weight)
+#' all(expected_g4_weights == actual_g4_weights)
 #'
-#' g5 <- graph_from_adjacency_matrix(adjm, weighted = TRUE, mode = "min")
-#' all(nzs(pmin(adjm, t(adjm))[upper.tri(adjm)]) == sort(E(g5)$weight))
+#' g5 <- graph_from_adjacency_matrix(
+#'   adj_matrix,
+#'   weighted = TRUE,
+#'   mode = "min"
+#' )
+#' expected_g5_weights <- non_zero_sort(
+#'   pmin(adj_matrix, t(adj_matrix))[upper.tri(adj_matrix, diag = TRUE)]
+#' )
+#' actual_g5_weights <- sort(E(g5)$weight)
+#' all(expected_g5_weights == actual_g5_weights)
 #'
-#' g6 <- graph_from_adjacency_matrix(adjm, weighted = TRUE, mode = "upper")
-#' all(nzs(adjm[upper.tri(adjm)]) == sort(E(g6)$weight))
+#' g6 <- graph_from_adjacency_matrix(
+#'   adj_matrix,
+#'   weighted = TRUE,
+#'   mode = "upper"
+#' )
+#' expected_g6_weights <- non_zero_sort(adj_matrix[upper.tri(adj_matrix, diag = TRUE)])
+#' actual_g6_weights <- sort(E(g6)$weight)
+#' all(expected_g6_weights == actual_g6_weights)
 #'
-#' g7 <- graph_from_adjacency_matrix(adjm, weighted = TRUE, mode = "lower")
-#' all(nzs(adjm[lower.tri(adjm)]) == sort(E(g7)$weight))
+#' g7 <- graph_from_adjacency_matrix(
+#'   adj_matrix,
+#'   weighted = TRUE,
+#'   mode = "lower"
+#' )
+#' expected_g7_weights <- non_zero_sort(adj_matrix[lower.tri(adj_matrix, diag = TRUE)])
+#' actual_g7_weights <- sort(E(g7)$weight)
+#' all(expected_g7_weights == actual_g7_weights)
 #'
-#' g8 <- graph_from_adjacency_matrix(adjm, weighted = TRUE, mode = "plus")
-#' d2 <- function(x) {
+#' g8 <- graph_from_adjacency_matrix(
+#'   adj_matrix,
+#'   weighted = TRUE,
+#'   mode = "plus"
+#' )
+#' halve_diag <- function(x) {
 #'   diag(x) <- diag(x) / 2
 #'   x
 #' }
-#' all(nzs((d2(adjm + t(adjm)))[lower.tri(adjm)]) == sort(E(g8)$weight))
+#' expected_g8_weights <- non_zero_sort(
+#'   halve_diag(adj_matrix + t(adj_matrix)
+#' )[lower.tri(adj_matrix, diag = TRUE)])
+#' actual_g8_weights <- sort(E(g8)$weight)
+#' all(expected_g8_weights == actual_g8_weights)
 #'
-#' g9 <- graph_from_adjacency_matrix(adjm, weighted = TRUE, mode = "plus", diag = FALSE)
-#' d0 <- function(x) {
+#' g9 <- graph_from_adjacency_matrix(
+#'   adj_matrix,
+#'   weighted = TRUE,
+#'   mode = "plus",
+#'   diag = FALSE
+#' )
+#' zero_diag <- function(x) {
 #'   diag(x) <- 0
 #' }
-#' all(nzs((d0(adjm + t(adjm)))[lower.tri(adjm)]) == sort(E(g9)$weight))
+#' expected_g9_weights <- non_zero_sort((zero_diag(adj_matrix + t(adj_matrix)))[lower.tri(adj_matrix)])
+#' actual_g9_weights <- sort(E(g9)$weight)
+#' all(expected_g9_weights == actual_g9_weights)
 #'
 #' ## row/column names
-#' rownames(adjm) <- sample(letters, nrow(adjm))
-#' colnames(adjm) <- seq(ncol(adjm))
-#' g10 <- graph_from_adjacency_matrix(adjm, weighted = TRUE, add.rownames = "code")
+#' rownames(adj_matrix) <- sample(letters, nrow(adj_matrix))
+#' colnames(adj_matrix) <- seq(ncol(adj_matrix))
+#' g10 <- graph_from_adjacency_matrix(
+#'   adj_matrix,
+#'   weighted = TRUE,
+#'   add.rownames = "code"
+#' )
 #' summary(g10)
 #'
 #' @export
