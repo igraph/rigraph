505ee039ac9bf910eea363ac21268813419d99bc
Antonov548 <antonov5551998@gmail.com>
2024-03-12 10:16:38 +0100

    `upstream_dir`

--- tools/update-cigraph.sh
+++ tools/update-cigraph.sh
@@ -7,9 +7,9 @@ set -o pipefail
 cd $(dirname $0)/..
 
 if [ -z "$1" ]; then
-  igraphdir=../../igraph
+  upstream_dir=../../igraph
 else
-  igraphdir="$1"
+  upstream_dir="$1"
 fi
 
 if [ -n "$(git status --porcelain)" ]; then
@@ -17,11 +17,11 @@ if [ -n "$(git status --porcelain)" ]; then
   exit 1
 fi
 
-if [ -n "$(git -C "$igraphdir" status --porcelain)" ]; then
-  echo "Warning: working directory $igraphdir not clean"
+if [ -n "$(git -C "$upstream_dir" status --porcelain)" ]; then
+  echo "Warning: working directory $upstream_dir not clean"
 fi
 
-commit=$(git -C "$igraphdir" rev-parse HEAD)
+commit=$(git -C "$upstream_dir" rev-parse HEAD)
 echo "Importing commit $commit"
 
 base=$(git log -n 1 --format="%s" -- src/vendor/cigraph | sed -r 's#^.*igraph/igraph@(.*)$|^.*$#\1#')
@@ -29,7 +29,7 @@ base=$(git log -n 1 --format="%s" -- src/vendor/cigraph | sed -r 's#^.*igraph/ig
 rm -rf src/vendor/cigraph
 mkdir -p src/vendor/cigraph
 
-git clone "$igraphdir" src/vendor/cigraph
+git clone "$upstream_dir" src/vendor/cigraph
 
 cmake -Ssrc/vendor/cigraph -Bsrc/vendor/cigraph/build
 
@@ -52,7 +52,7 @@ git add src/vendor src/*.mk R/aaa-auto.R src/cpp11.cpp src/rinterface.c
 (
   echo "chore: Update vendored sources to igraph/igraph@$commit"
   echo
-  git -C "$igraphdir" log --first-parent --format="%s" ${base}..${commit} | sed -r 's%(#[0-9]+)%igraph/igraph\1%g'
+  git -C "$upstream_dir" log --first-parent --format="%s" ${base}..${commit} | sed -r 's%(#[0-9]+)%igraph/igraph\1%g'
 ) | git commit --file /dev/stdin
 
 git cherry-pick b6f576d25dc2c02dfeb2979716ef4809e4404c99 --no-edit
