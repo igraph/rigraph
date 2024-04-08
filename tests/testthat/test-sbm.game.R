44cb71896cfdae53bd2cd07fa5e9078a3409dce0
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-04-08 14:47:52 +0200

    refactor: remove deprecated expect_that() from test-sbm.game.R

--- tests/testthat/test-sbm.game.R
+++ tests/testthat/test-sbm.game.R
@@ -12,19 +12,19 @@ test_that("Generating stochastic block models works", {
     directed = FALSE, loops = TRUE
   )
   g2x <- make_full_graph(10, directed = FALSE, loops = TRUE)
-  expect_that(g2[sparse = FALSE], equals(g2x[sparse = FALSE]))
+  expect_equal(g2[sparse = FALSE], g2x[sparse = FALSE])
 
   g3 <- sample_sbm(10,
     pref.matrix = pm, block.sizes = bs,
     directed = TRUE, loops = FALSE
   )
   g3x <- make_full_graph(10, directed = TRUE, loops = FALSE)
-  expect_that(g3[sparse = FALSE], equals(g3x[sparse = FALSE]))
+  expect_equal(g3[sparse = FALSE], g3x[sparse = FALSE])
 
   g4 <- sample_sbm(10,
     pref.matrix = pm, block.sizes = bs,
     directed = TRUE, loops = TRUE
   )
   g4x <- make_full_graph(10, directed = TRUE, loops = TRUE)
-  expect_that(g4[sparse = FALSE], equals(g4x[sparse = FALSE]))
+  expect_equal(g4[sparse = FALSE], g4x[sparse = FALSE])
 })
