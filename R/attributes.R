dad6e67366dbc9842305cc45b05e328953d33337
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-04-08 12:26:26 +0200

    Update R/attributes.R
    Co-authored-by: Kirill Müller <krlmlr@users.noreply.github.com>

--- R/attributes.R
+++ R/attributes.R
@@ -518,9 +518,8 @@ vertex.attributes <- function(graph, index = V(graph)) {
   res <- .Call(R_igraph_mybracket2_copy, graph, igraph_t_idx_attr, igraph_attr_idx_vertex)
 
   if (!missing(index)) {
-    index_length_not_vertices_number <- (length(index) != vcount(graph))
-    some_index_not_vertices <- (!all(index %in% V(graph)))
-    if  (index_length_not_vertices_number || some_index_not_vertices) {
+    index_is_natural_sequence <- (length(index) == vcount(graph) && all(index == V(graph)))
+    if  (!index_is_natural_sequence) {
       for (i in seq_along(res)) {
         res[[i]] <- res[[i]][index]
       }
