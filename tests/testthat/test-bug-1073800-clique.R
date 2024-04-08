5ad30896ca2fd716d94f8c759aa72e77f041a9ed
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-04-08 14:43:05 +0200

    refactor: remove deprecated expect_that() from test-bug-1073800-clique.R

--- tests/testthat/test-bug-1073800-clique.R
+++ tests/testthat/test-bug-1073800-clique.R
@@ -3,5 +3,5 @@ test_that("Largest cliques is correct", {
   adj <- matrix(1, nrow = 11, ncol = 11) - diag(11)
   g <- graph_from_adjacency_matrix(adj)
   lc <- suppressWarnings(largest_cliques(g))
-  expect_that(unvs(lc), equals(list(1:11)))
+  expect_equal(unvs(lc), list(1:11))
 })
