551b6ef647384c9c6a3f76f7ee2e5c2e8743794c
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-04-08 14:46:08 +0200

    refactor: remove deprecated expect_that() from test-handler.R

--- tests/testthat/test-handler.R
+++ tests/testthat/test-handler.R
@@ -1,5 +1,7 @@
 test_that("can create graphs when igraph is not attached", {
-  g <- callr::r(function() { igraph::make_ring(3, directed = TRUE) })
+  g <- callr::r(function() {
+    igraph::make_ring(3, directed = TRUE)
+  })
   g2 <- make_ring(3, directed = TRUE)
   expect_identical(
     unclass(g)[-igraph_t_idx_env],
