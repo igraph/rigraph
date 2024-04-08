6be4837fa194cbb33d86111ec7d8485f494583f2
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-04-08 14:46:26 +0200

    refactor: remove deprecated expect_that() from test-is.bipartite.R

--- tests/testthat/test-is.bipartite.R
+++ tests/testthat/test-is.bipartite.R
@@ -6,8 +6,8 @@ test_that("is_bipartite works", {
   withr::local_seed(42)
   I <- matrix(sample(0:1, 35, replace = TRUE, prob = c(3, 1)), ncol = 5)
   g <- graph_from_biadjacency_matrix(I)
-  expect_that(
+  expect_equal(
     bipartite_mapping(g),
-    equals(list(res = TRUE, type = c(rep(FALSE, 7), rep(TRUE, 5))))
+    list(res = TRUE, type = c(rep(FALSE, 7), rep(TRUE, 5)))
   )
 })
