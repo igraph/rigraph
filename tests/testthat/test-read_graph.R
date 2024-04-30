3476e3e7265d2ef234d41ae8fbdcee6c3d39d4d0
Szabolcs Horvát <szhorvat@gmail.com>
2024-04-30 13:14:38 +0000

    Fix tests

--- tests/testthat/test-read_graph.R
+++ tests/testthat/test-read_graph.R
@@ -7,16 +7,16 @@ test_that("reading GraphML file works", {
   expect_true(isomorphic(g2, g))
 })
 
-test_that("reading graph in ncol format", {
-  ncol_path <- tempfile("zoo", fileext = ".ncol")
+test_that("reading graph in NCOL format", {
+  ncol_path <- tempfile("testfile", fileext = ".ncol")
   g <- make_graph(c(1, 2, 2, 3))
-  write_graph(g, "zoo.ncol", "ncol")
+  write_graph(g, ncol_path, "ncol")
   expect_no_error(g2 <- read_graph(ncol_path, "ncol"))
 })
 
-test_that("reading graph in lgl format", {
-  ncol_path <- tempfile("zoo", fileext = ".ncol")
+test_that("reading graph in LGL format", {
+  lgl_path <- tempfile("testfile", fileext = ".lgl")
   g <- make_graph(c(1, 2, 2, 3))
-  write_graph(g, "zoo.lgl", "lgl")
-  expect_no_error(g2 <- read_graph(ncol_path, "lgl"))
+  write_graph(g, lgl_path, "lgl")
+  expect_no_error(g2 <- read_graph(lgl_path, "lgl"))
 })
