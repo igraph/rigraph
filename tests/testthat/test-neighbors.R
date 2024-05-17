0e752417ada8e844392414a9a52538f0a54c7d37
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-05-17 13:52:50 +0200

    test: improve neighbors() tests
    fix

--- tests/testthat/test-neighbors.R
+++ tests/testthat/test-neighbors.R
@@ -1,9 +1,9 @@
 test_that("neighbors works", {
   g <- sample_gnp(100, 20 / 100)
   al <- as_adj_list(g, mode = "all")
-  for (i in 1:length(al)) {
-    n <- neighbors(g, i, mode = "out")
-    expect_that(sort(n), is_equivalent_to(al[[i]]))
+  for (i in seq_along(al)) {
+    n <- neighbors(g, v = i, mode = "out")
+    expect_setequal(sort(n), al[[i]])
   }
 })
 
