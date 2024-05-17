6a45ac042eb389dc37dd3d76da22b98f16ccbe58
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-05-17 13:21:15 +0200

    test: improve add_vertices() test

--- tests/testthat/test-delete.vertices.R
+++ tests/testthat/test-delete.vertices.R
@@ -2,7 +2,8 @@ test_that("delete_vertices works", {
   g <- graph_from_literal(A:B:C - D:E:F, D - E - F)
 
   g2 <- delete_vertices(g, "A")
-  g3 <- delete_vertices(g, match("A", V(g)$name))
+  expect_equal(V(g2)$name, c("B", "C", "D", "E", "F"))
 
+  g3 <- delete_vertices(g, match("A", V(g)$name))
   expect_true(graph.isomorphic(g2, g3))
 })
