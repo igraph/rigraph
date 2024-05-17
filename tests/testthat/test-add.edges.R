d070bafb05fa3922742fbc22160db1364ff0732d
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-05-17 13:38:51 +0200

    test: improve add_edges() test

--- tests/testthat/test-add.edges.R
+++ tests/testthat/test-add.edges.R
@@ -2,44 +2,44 @@ test_that("add_edges keeps edge id order", {
   g <- make_empty_graph(10)
   edges <- c(1, 2, 2, 3, 3, 4, 1, 6, 1, 7, 9, 10)
   g2 <- add_edges(g, edges)
-  ec <- ecount(g2)
-  ec2 <- length(edges) / 2
-  expect_equal(ec, ec2)
+
+  expect_equal(ecount(g2), length(edges) / 2)
   expect_equal(get.edge.ids(g2, edges), seq_len(length(edges) / 2))
 })
 
 test_that("add_edges adds attributes", {
   g <- make_empty_graph(10)
-  g3 <- add_edges(g, (edges <- c(1, 5, 2, 6, 3, 10, 4, 5)),
-    attr = list(weight = (weights <- c(1, 2, 1, -1)))
-  )
-  expect_that(ecount(g3), equals(length(edges) / 2))
-  expect_that(get.edge.ids(g3, edges), equals(seq_len(length(edges) / 2)))
-  expect_that(E(g3)$weight, equals(weights))
+  edges <- c(1, 5, 2, 6, 3, 10, 4, 5)
+  weights <- c(1, 2, 1, -1)
+  g3 <- add_edges(g, edges, attr = list(weight = weights))
+
+  expect_equal(ecount(g3), (length(edges) / 2))
+  expect_equal(get.edge.ids(g3, edges), seq_len(length(edges) / 2))
+  expect_equal(E(g3)$weight, weights)
 })
 
 test_that("add_edges unknown attributes to NA", {
   g <- make_empty_graph(10)
-  g2 <- add_edges(g, (edges <- c(1, 2, 2, 3, 3, 4, 1, 6, 1, 7, 9, 10)))
+  edges <- c(1, 2, 2, 3, 3, 4, 1, 6, 1, 7, 9, 10)
+  g2 <- add_edges(g, edges)
   g4 <- add_edges(g2, c(1, 4, 4, 6, 7, 1), attr = list(weight = c(-1, 1, -2.5)))
+
   expect_true(all(is.na(E(g4)$weight[seq_len(length(edges) / 2)])))
 })
 
 test_that("add_edges appends attributes properly", {
   g <- make_empty_graph(10)
-  g3 <- add_edges(g, (edges1 <- c(1, 5, 2, 6, 3, 10, 4, 5)),
-    attr = list(weight = (weights1 <- c(1, 2, 1, -1)))
-  )
-  g5 <- add_edges(g3, (edges2 <- c(10, 9, 10, 10, 1, 1)),
-    attr = list(weight = (weights2 <- c(100, 100, 100)))
-  )
-  expect_that(E(g5)$weight, equals(c(weights1, weights2)))
+  edges1 <- c(1, 5, 2, 6, 3, 10, 4, 5)
+  weights1 <- c(1, 2, 1, -1)
+  g2 <- add_edges(g, edges1, attr = list(weight = weights1))
+
+  edges2 <- c(10, 9, 10, 10, 1, 1)
+  weights2 <- c(100, 100, 100)
+  g3 <- add_edges(g2, edges2, attr = list(weight = weights2))
+  expect_equal(E(g3)$weight, c(weights1, weights2))
 })
 
 test_that("add_edges signals error for zero vertex ids", {
   g <- make_full_graph(5) %du% make_full_graph(5) %du% make_full_graph(5)
-  expect_that(
-    add_edges(g, c(0, 5, 0, 10, 5, 10)),
-    throws_error("Invalid vertex")
-  )
+  expect_error(add_edges(g, c(0, 5, 0, 10, 5, 10)), "Invalid vertex")
 })
