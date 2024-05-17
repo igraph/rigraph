b7d75f08ebae1991606c1433af50a5e96e7276b4
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-05-17 14:12:25 +0200

    test: put all tests of interface.R in a single test file

--- tests/testthat/test-delete.vertices.R
+++
@@ -1,9 +0,0 @@
-test_that("delete_vertices works", {
-  g <- graph_from_literal(A:B:C - D:E:F, D - E - F)
-
-  g2 <- delete_vertices(g, "A")
-  expect_equal(V(g2)$name, c("B", "C", "D", "E", "F"))
-
-  g3 <- delete_vertices(g, match("A", V(g)$name))
-  expect_isomorphic(g2, g3)
-})
