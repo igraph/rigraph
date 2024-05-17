7d97190845794a44f96823a039e8e1b94c92f6e4
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-05-17 13:27:17 +0200

    test: improve delete_edges test

--- tests/testthat/test-delete.edges.R
+++ tests/testthat/test-delete.edges.R
@@ -1,12 +1,20 @@
 test_that("delete_edges works", {
   g <- graph_from_literal(A:B:C - D:E:F, D - E - F)
   g2 <- delete_edges(g, E(g, P = c("D", "E")))
-  expect_that(
-    as.matrix(g2[]),
-    is_equivalent_to(cbind(
-      c(0, 0, 0, 1, 1, 1), c(0, 0, 0, 1, 1, 1),
-      c(0, 0, 0, 1, 1, 1), c(1, 1, 1, 0, 0, 0),
-      c(1, 1, 1, 0, 0, 1), c(1, 1, 1, 0, 1, 0)
-    ))
-  )
+
+  expected_matrix <- matrix(
+      c(
+        0, 0, 0, 1, 1, 1,
+        0, 0, 0, 1, 1, 1,
+        0, 0, 0, 1, 1, 1,
+        1, 1, 1, 0, 0, 0,
+        1, 1, 1, 0, 0, 1,
+        1, 1, 1, 0, 1, 0
+      ),
+      nrow = 6L,
+      ncol = 6L,
+      dimnames = list(c("A", "B", "C", "D", "E", "F"), c("A", "B", "C", "D", "E", "F"))
+    )
+
+  expect_equal(as.matrix(g2[]), expected_matrix)
 })
