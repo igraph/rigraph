f39f82fbc225e62568fde272077be3c70b17ddd2
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-05-23 08:50:10 +0200

    test: move test to correct test file

--- tests/testthat/test-adjacency.R
+++ tests/testthat/test-adjacency.R
@@ -36,3 +36,9 @@ test_that("is_symmetric() works for amat", {
   )
   expect_false(is_symmetric())
 })
+
+test_that("weighted graph_from_adjacency_matrix works on integer matrices", {
+  data <- matrix(c(0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0), 4)
+  g <- graph_from_adjacency_matrix(data, weighted = TRUE)
+  expect_that(as.matrix(g[]), is_equivalent_to(data))
+})
