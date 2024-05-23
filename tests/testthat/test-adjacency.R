5f05a6adb9cbf2274f78c6b9b6e182fcacfc6a37
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-05-23 09:16:36 +0200

    test: expect_equal() rather than expect_that(is_equivalent_to())

--- tests/testthat/test-adjacency.R
+++ tests/testthat/test-adjacency.R
@@ -654,5 +654,5 @@ test_that("graph_from_adjacency_matrix works for sparse matrices without values"
 test_that("weighted graph_from_adjacency_matrix() works on integer matrices", {
   data <- matrix(c(0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0), 4)
   g <- graph_from_adjacency_matrix(data, weighted = TRUE)
-  expect_that(as.matrix(g[]), is_equivalent_to(data))
+  expect_equal(as.matrix(g[]), data)
 })
