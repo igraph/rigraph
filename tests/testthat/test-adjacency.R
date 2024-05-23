ada30a8eb992e5915b5e4de005b37a4762844d05
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-05-23 08:50:10 +0200

    test: move test to correct test file

--- tests/testthat/test-adjacency.R
+++ tests/testthat/test-adjacency.R
@@ -656,3 +656,9 @@ test_that("weighted graph_from_adjacency_matrix() works on integer matrices", {
   g <- graph_from_adjacency_matrix(data, weighted = TRUE)
   expect_equal(as.matrix(g[]), data)
 })
+
+test_that("weighted graph_from_adjacency_matrix works on integer matrices", {
+  data <- matrix(c(0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0), 4)
+  g <- graph_from_adjacency_matrix(data, weighted = TRUE)
+  expect_that(as.matrix(g[]), is_equivalent_to(data))
+})
