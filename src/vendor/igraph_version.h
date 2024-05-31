809451fe3f439aaae15cae12f8d94cd87a134783
Antonov548 <antonov5551998@gmail.com>
2024-05-31 12:19:05 +0200

    update cigraph

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.12-96-gfb61db743"
+#define IGRAPH_VERSION "0.10.12-98-g34d078f1e"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 12
-#define IGRAPH_VERSION_PRERELEASE "96-gfb61db743"
+#define IGRAPH_VERSION_PRERELEASE "98-g34d078f1e"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
