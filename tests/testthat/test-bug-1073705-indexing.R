c47dc989914a8f58cc17bb1fa453018ae9c42147
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-06-04 15:24:35 +0200

    refactor: remove deprecated expect_that() from test-bug-1073705-indexing.R

--- tests/testthat/test-bug-1073705-indexing.R
+++ tests/testthat/test-bug-1073705-indexing.R
@@ -2,15 +2,15 @@ test_that("Weighted indexing does not remove edges", {
   g <- make_ring(10)
   g[1, 2, attr = "weight"] <- 0
   expect_true("weight" %in% edge_attr_names(g))
-  expect_that(E(g)$weight, equals(c(0, rep(NA, 9))))
+  expect_equal(E(g)$weight, c(0, rep(NA, 9)))
 
   el <- as_edgelist(g)
   g[from = el[, 1], to = el[, 2], attr = "sim"] <- rep(0:1, length.out = ecount(g))
   expect_true("sim" %in% edge_attr_names(g))
-  expect_that(E(g)$sim, equals(rep(0:1, 5)))
+  expect_equal(E(g)$sim, rep(0:1, 5))
 
   V(g)$name <- letters[seq_len(vcount(g))]
   el <- as_edgelist(g)
   g[from = el[, 1], to = el[, 2], attr = "sim"] <- rep(1:0, length.out = ecount(g))
-  expect_that(E(g)$sim, equals(rep(1:0, 5)))
+  expect_equal(E(g)$sim, rep(1:0, 5))
 })
