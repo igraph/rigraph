e8aee0c30847c0ec877d787d69d645e0da9da981
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-06-04 15:28:46 +0200

    refactor: remove deprecated expect_that() from test-graph.density.R

--- tests/testthat/test-graph.density.R
+++ tests/testthat/test-graph.density.R
@@ -2,12 +2,12 @@ test_that("edge_density works", {
   g <- sample_gnp(50, 4 / 50)
   gd <- edge_density(g)
   gd2 <- ecount(g) / vcount(g) / (vcount(g) - 1) * 2
-  expect_that(gd, equals(gd2))
+  expect_equal(gd, gd2)
 
   ####
 
   g <- sample_gnp(50, 4 / 50, directed = TRUE)
   gd <- edge_density(g)
   gd2 <- ecount(g) / vcount(g) / (vcount(g) - 1)
-  expect_that(gd, equals(gd2))
+  expect_equal(gd, gd2)
 })
