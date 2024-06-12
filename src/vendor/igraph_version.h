217c9500be599fd4b6851c805a6bffe0bcb19e04
Szabolcs Horvát <szhorvat@gmail.com>
2024-06-12 10:05:32 -0400

    chore: Update vendored sources to igraph/igraph@2048f8e65f695c53d9a255c334bb69f9a3616df1
    Merge pull request igraph/igraph#2618 from igraph/experiment/intersection-benchmark
    refactor: more use of vector_null()
    refactor: minor cleanup in is_connected()
    fuzzing: include igraph_ecc()
    doc: improve igraph_es_all() docs
    Merge pull request igraph/igraph#2624 from igraph/feat/bitset-tests
    chore: order changelog by relevance [skip ci]
    doc: update arpack starting vector claim in hub_and_authority_scores()

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.12-144-g7ed72499d"
+#define IGRAPH_VERSION "0.10.12-158-g2048f8e65"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 12
-#define IGRAPH_VERSION_PRERELEASE "144-g7ed72499d"
+#define IGRAPH_VERSION_PRERELEASE "158-g2048f8e65"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
