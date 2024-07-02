12f2d2073537676b01381242994c4b5296d04477
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-07-02 11:10:19 +0200

    test: rename test objects to make names unique

--- tests/testthat/test-centrality.R
+++ tests/testthat/test-centrality.R
@@ -1,15 +1,15 @@
 test_that("subgraph_centrality works", {
-  g <- make_graph("Frucht")
+  frucht_graph <- make_graph("Frucht")
   expect_equal(
-    subgraph_centrality(g),
-    Matrix::diag(Matrix::expm(as_adj(g, sparse = FALSE))),
+    subgraph_centrality(frucht_graph),
+    Matrix::diag(Matrix::expm(as_adj(frucht_graph, sparse = FALSE))),
     tolerance = 1e-10
   )
 
-  g <- make_graph("Grotzsch")
+  grotzsch_graph <- make_graph("Grotzsch")
   expect_equal(
-    subgraph_centrality(g),
-    Matrix::diag(Matrix::expm(as_adj(g, sparse = FALSE))),
+    subgraph_centrality(grotzsch_graph),
+    Matrix::diag(Matrix::expm(as_adj(grotzsch_graph, sparse = FALSE))),
     tolerance = 1e-10
   )
 })
