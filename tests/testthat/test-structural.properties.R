a796689efb54c81408357fa55d76ed77c894442d
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-17 15:42:28 +0200

    test: fold farthest_vertices() tests into test-structural.properties

--- tests/testthat/test-structural.properties.R
+++ tests/testthat/test-structural.properties.R
@@ -209,3 +209,28 @@ test_that("get_diameter() works", {
   length(gd) == d + 1
   expect_equal(sp[gd[1], gd[length(gd)]], d)
 })
+
+test_that("farthest_vertices() works", {
+  kite <- graph_from_literal(
+    Andre - Beverly:Carol:Diane:Fernando,
+    Beverly - Andre:Diane:Ed:Garth,
+    Carol - Andre:Diane:Fernando,
+    Diane - Andre:Beverly:Carol:Ed:Fernando:Garth,
+    Ed - Beverly:Diane:Garth,
+    Fernando - Andre:Carol:Diane:Garth:Heather,
+    Garth - Beverly:Diane:Ed:Fernando:Heather,
+    Heather - Fernando:Garth:Ike,
+    Ike - Heather:Jane,
+    Jane - Ike
+  )
+
+  fn <- farthest_vertices(kite)
+  fn$vertices <- as.vector(fn$vertices)
+  expect_equal(fn, list(vertices = c(1, 10), distance = 4))
+
+  expect_equal(
+    distances(kite, v = fn$vertices[1], to = fn$vertices[2])[1],
+    fn$distance
+  )
+  expect_equal(diameter(kite), fn$distance)
+})
