d0c750fb615b55eb1ae9de9c2c63b58ceeff5b71
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-17 15:35:43 +0200

    test: fold test-graph.dfs into test-structural.properties

--- tests/testthat/test-structural.properties.R
+++ tests/testthat/test-structural.properties.R
@@ -1,9 +1,9 @@
-test_that("DFS uses 1-based root vertex index", {
+test_that("dfs() uses 1-based root vertex index", {
   g <- make_ring(3)
   expect_equal(dfs(g, root = 1)$root, 1)
 })
 
-test_that("DFS does not pad order", {
+test_that("dfs() does not pad order", {
   g <- make_star(3)
   expect_equal(as.numeric(dfs(g, root = 2, unreachable = FALSE)$order), c(2, 1))
 
