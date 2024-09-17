2da0d200cf268b082868419d70a4554d42710049
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-17 14:23:58 +0200

    test: use expect_silent() instead of expect_warning(regexp = NA)

--- tests/testthat/test-layout_null_singleton.R
+++ tests/testthat/test-layout_null_singleton.R
@@ -3,49 +3,49 @@ test_that("layout algorithms work for null graphs", {
   mat <- matrix(as.numeric(c()), ncol = 2)
   mat3 <- matrix(as.numeric(c()), ncol = 3)
 
-  expect_warning(layout_as_tree(g), regexp = NA)
+  expect_silent(layout_as_tree(g))
   expect_equal(mat, layout_as_tree(g))
 
-  expect_warning(layout_as_star(g), regexp = NA)
+  expect_silent(layout_as_star(g))
   expect_equal(mat, layout_as_star(g))
 
-  expect_warning(layout_in_circle(g), regexp = NA)
+  expect_silent(layout_in_circle(g))
   expect_equal(mat, layout_in_circle(g))
 
-  expect_warning(layout_nicely(g), regexp = NA)
+  expect_silent(layout_nicely(g))
   expect_equal(mat, layout_nicely(g))
 
-  expect_warning(layout_on_grid(g), regexp = NA)
+  expect_silent(layout_on_grid(g))
   expect_equal(mat, layout_on_grid(g))
 
-  expect_warning(layout_on_sphere(g), regexp = NA)
+  expect_silent(layout_on_sphere(g))
   expect_equal(mat3, layout_on_sphere(g))
 
-  expect_warning(layout_randomly(g), regexp = NA)
+  expect_silent(layout_randomly(g))
   expect_equal(mat, layout_randomly(g))
 
-  expect_warning(layout_with_dh(g), regexp = NA)
+  expect_silent(layout_with_dh(g))
   expect_equal(mat, layout_with_dh(g))
 
-  expect_warning(layout_with_fr(g), regexp = NA)
+  expect_silent(layout_with_fr(g))
   expect_equal(mat, layout_with_fr(g))
 
-  expect_warning(layout_with_gem(g), regexp = NA)
+  expect_silent(layout_with_gem(g))
   expect_equal(mat, layout_with_gem(g))
 
-  expect_warning(layout_with_graphopt(g), regexp = NA)
+  expect_silent(layout_with_graphopt(g))
   expect_equal(mat, layout_with_graphopt(g))
 
-  expect_warning(layout_with_kk(g), regexp = NA)
+  expect_silent(layout_with_kk(g))
   expect_equal(mat, layout_with_kk(g))
 
-  expect_warning(layout_with_lgl(g), regexp = NA)
+  expect_silent(layout_with_lgl(g))
   expect_equal(mat, layout_with_lgl(g))
 
-  expect_warning(layout_with_mds(g), regexp = NA)
+  expect_silent(layout_with_mds(g))
   expect_equal(mat, layout_with_mds(g))
 
-  expect_warning(layout_with_sugiyama(g), regexp = NA)
+  expect_silent(layout_with_sugiyama(g))
   expect_equal(mat, layout_with_sugiyama(g)$layout)
   expect_equal(mat, layout_with_sugiyama(g)$layout.dummy)
 })
@@ -60,46 +60,46 @@ test_that("layout algorithms work for singleton graphs", {
     }
   }
 
-  expect_warning(layout_as_tree(g), regexp = NA)
+  expect_silent(layout_as_tree(g))
   check_matrix(layout_as_tree(g))
 
-  expect_warning(layout_as_star(g), regexp = NA)
+  expect_silent(layout_as_star(g))
   check_matrix(layout_as_star(g))
 
-  expect_warning(layout_in_circle(g), regexp = NA)
+  expect_silent(layout_in_circle(g))
   check_matrix(layout_in_circle(g))
 
-  expect_warning(layout_nicely(g), regexp = NA)
+  expect_silent(layout_nicely(g))
   check_matrix(layout_nicely(g))
 
-  expect_warning(layout_on_grid(g), regexp = NA)
+  expect_silent(layout_on_grid(g))
   check_matrix(layout_on_grid(g))
 
-  expect_warning(layout_on_sphere(g), regexp = NA)
+  expect_silent(layout_on_sphere(g))
   check_matrix(layout_on_sphere(g), ncol = 3)
 
-  expect_warning(layout_randomly(g), regexp = NA)
+  expect_silent(layout_randomly(g))
   check_matrix(layout_randomly(g))
 
-  expect_warning(layout_with_dh(g), regexp = NA)
+  expect_silent(layout_with_dh(g))
   check_matrix(layout_with_dh(g))
 
-  expect_warning(layout_with_fr(g), regexp = NA)
+  expect_silent(layout_with_fr(g))
   check_matrix(layout_with_fr(g))
 
-  expect_warning(layout_with_gem(g), regexp = NA)
+  expect_silent(layout_with_gem(g))
   check_matrix(layout_with_gem(g))
 
-  expect_warning(layout_with_graphopt(g), regexp = NA)
+  expect_silent(layout_with_graphopt(g))
   check_matrix(layout_with_graphopt(g))
 
-  expect_warning(layout_with_kk(g), regexp = NA)
+  expect_silent(layout_with_kk(g))
   check_matrix(layout_with_kk(g))
 
-  expect_warning(layout_with_lgl(g), regexp = NA)
+  expect_silent(layout_with_lgl(g))
   check_matrix(layout_with_lgl(g))
 
-  expect_warning(layout_with_sugiyama(g), regexp = NA)
+  expect_silent(layout_with_sugiyama(g))
   check_matrix(layout_with_sugiyama(g)$layout)
   check_matrix(layout_with_sugiyama(g)$layout.dummy, nrow = 0)
 })
