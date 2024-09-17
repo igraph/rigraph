7c59487c007b464052fef307d7d79932bf9659c8
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-17 15:51:12 +0200

    test: fold constraint() test into test-structural.properties

--- tests/testthat/test-structural.properties.R
+++ tests/testthat/test-structural.properties.R
@@ -477,3 +477,43 @@ test_that("local transitivity() produces named vectors", {
   expect_equal(names(t3), vs)
   expect_equal(names(t4), vs)
 })
+
+test_that("constraint() works", {
+  constraint.orig <- function(graph, nodes = V(graph), attr = NULL) {
+    ensure_igraph(graph)
+    idx <- degree(graph) != 0
+    A <- as_adj(graph, attr = attr, sparse = FALSE)
+    A <- A[idx, idx]
+    n <- sum(idx)
+
+    one <- c(rep(1, n))
+    CZ <- A + t(A)
+    cs <- CZ %*% one # degree of vertices
+    ics <- 1 / cs
+    CS <- ics %*% t(one) # 1/degree of vertices
+    P <- CZ * CS # intermediate result: proportionate tie strengths
+    PSQ <- P %*% P # sum paths of length two
+    P.bi <- as.numeric(P > 0) # exclude paths to non-contacts (& reflexive):
+    PC <- (P + (PSQ * P.bi))^2 # dyadic constraint
+    ci <- PC %*% one # overall constraint
+    dim(ci) <- NULL
+
+    ci2 <- numeric(vcount(graph))
+    ci2[idx] <- ci
+    ci2[!idx] <- NaN
+    ci2[nodes]
+  }
+
+  karate <- make_graph("Zachary")
+
+  c1 <- constraint(karate)
+  c2 <- constraint.orig(karate)
+  expect_equal(c1, c2)
+
+  withr::local_seed(42)
+  E(karate)$weight <- sample(1:10, replace = TRUE, ecount(karate))
+  wc1 <- constraint(karate)
+  wc2 <- constraint.orig(karate, attr = "weight")
+  expect_equal(wc1, wc2)
+})
+
