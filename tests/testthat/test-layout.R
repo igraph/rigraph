bfb24e6496b6f75c911d54579038ffe6b60d225e
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-17 14:28:09 +0200

    test: fold test-layout.sugiyama into test-layout

--- tests/testthat/test-layout.R
+++ tests/testthat/test-layout.R
@@ -185,3 +185,9 @@ test_that("Kamada-Kawai layout generator works", {
   l <- layout_with_kk(g, maxiter = 5000, dim = 3)
   expect_true(looks_circular(l[-1,], check_dists = FALSE, eps = 1e-2))
 })
+
+test_that("layout_with_sugiyama() does not demote matrices to vectors in res$layout.dummy", {
+  ex <- graph_from_literal(A - +B:C, B - +C:D)
+  layex <- layout_with_sugiyama(ex, layers = NULL)
+  expect_equal(nrow(layex$layout.dummy), 1)
+})
