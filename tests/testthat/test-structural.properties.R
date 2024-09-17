164b1408116b6cc7b7c1feaca8078eea98ae4bbc
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-17 15:57:50 +0200

    test: fold unfold_tree() tests into test-structural.properties

--- tests/testthat/test-structural.properties.R
+++ tests/testthat/test-structural.properties.R
@@ -648,3 +648,15 @@ test_that("max_bipartite_match() handles missing types gracefully", {
   g <- graph_from_data_frame(df)
   expect_error(max_bipartite_match(g), "supply .*types.* argument")
 })
+
+test_that("unfold_tree() works", {
+  g <- make_tree(7, 2)
+  g <- add_edges(g, c(2, 7, 1, 4))
+  g2 <- unfold_tree(g, roots = 1)
+  expect_isomorphic(g2$tree, make_graph(c(
+    1, 2, 1, 3, 2, 8, 2, 5, 3, 6,
+    3, 9, 2, 7, 1, 4
+  )))
+  expect_equal(g2$vertex_index, c(1, 2, 3, 4, 5, 6, 7, 4, 7))
+})
+
