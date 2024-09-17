b1374232c7e9e914dc9d6fba68de240d9e450698
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-17 15:59:20 +0200

    test: fold test-girth into test-structural.properties

--- tests/testthat/test-structural.properties.R
+++ tests/testthat/test-structural.properties.R
@@ -727,3 +727,17 @@ test_that("the largest component of a null graph is a valid null graph", {
 
   expect_true(isomorphic(largest_component(make_empty_graph(0)), nullgraph))
 })
+
+test_that("girth() works", {
+  ## No circle in a tree
+  g <- make_tree(1000, 3)
+  gi <- girth(g)
+  expect_equal(gi$girth, Inf)
+  expect_equal(as.vector(gi$circle), numeric())
+
+  ## The worst case running time is for a ring
+  g <- make_ring(100)
+  gi <- girth(g)
+  expect_equal(gi$girth, 100)
+  expect_equal(sort(diff(as.vector(gi$circle))), c(-99, rep(1, 98)))
+})
