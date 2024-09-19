0600039d2ca7dba031ffd012ffc02350adba4142
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-19 15:19:29 +0200

    test: fold tests of as_directed() and as_undirected() into test-conversion

--- tests/testthat/test-conversion.R
+++ tests/testthat/test-conversion.R
@@ -48,3 +48,24 @@ test_that("as.undirected() deprecation", {
   g <- sample_gnp(100, 2 / 100)
   expect_snapshot(is_directed(as.undirected(g, mode = "collapse")))
 })
+
+test_that("as_undirected() keeps attributes", {
+  g <- graph_from_literal(A + -+B, A - -+C, C + -+D)
+  g$name <- "Tiny graph"
+  E(g)$weight <- seq_len(ecount(g))
+
+  g2 <- as_undirected(g, mode = "collapse")
+  df2 <- as_data_frame(g2)
+  g3 <- as_undirected(g, mode = "each")
+  df3 <- as_data_frame(g3)
+  g4 <- as_undirected(g, mode = "mutual")
+  df4 <- as_data_frame(g4)
+
+  expect_equal(g2$name, g$name)
+  expect_equal(g3$name, g$name)
+  expect_equal(g4$name, g$name)
+
+  expect_equal(df2[order(df2[, 1], df2[, 2]), ]$weight, c(4, 2, 9))
+  expect_equal(df3[order(df3[, 1], df3[, 2]), ]$weight, c(1, 3, 2, 4, 5))
+  expect_equal(df4[order(df4[, 1], df4[, 2]), ]$weight, c(4, 9))
+})
