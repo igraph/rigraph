908fc0230801397430bd166657461dfc939810fc
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-24 15:51:27 +0200

    test: fold closeness() test into test-centrality

--- tests/testthat/test-centrality.R
+++ tests/testthat/test-centrality.R
@@ -149,3 +149,50 @@ test_that("shortest paths are compared with tolerance when calculating betweenne
 
   expect_equal(result[1:5], c("1" = 0, "2" = 44, "3" = 71, "4" = 36.5, "6" = 44))
 })
+
+test_that("closeness works", {
+  kite <- graph_from_literal(
+    Andre - Beverly:Carol:Diane:Fernando,
+    Beverly - Andre:Diane:Ed:Garth,
+    Carol - Andre:Diane:Fernando,
+    Diane - Andre:Beverly:Carol:Ed:Fernando:Garth,
+    Ed - Beverly:Diane:Garth,
+    Fernando - Andre:Carol:Diane:Garth:Heather,
+    Garth - Beverly:Diane:Ed:Fernando:Heather,
+    Heather - Fernando:Garth:Ike,
+    Ike - Heather:Jane,
+    Jane - Ike
+  )
+
+  clo <- closeness(kite) * (vcount(kite) - 1)
+  expect_equal(
+    round(sort(clo, decreasing = TRUE), 3),
+    c(Fernando = 0.643, Garth = 0.643, Diane = 0.600, Heather = 0.600, Andre = 0.529, Beverly = 0.529, Carol = 0.500, Ed = 0.500, Ike = 0.429, Jane = 0.310)
+  )
+
+  clo2 <- closeness(kite, normalized = TRUE)
+  expect_equal(clo, clo2)
+})
+
+## TODO: weighted closeness
+
+test_that("closeness centralization works", {
+  kite <- graph_from_literal(
+    Andre - Beverly:Carol:Diane:Fernando,
+    Beverly - Andre:Diane:Ed:Garth,
+    Carol - Andre:Diane:Fernando,
+    Diane - Andre:Beverly:Carol:Ed:Fernando:Garth,
+    Ed - Beverly:Diane:Garth,
+    Fernando - Andre:Carol:Diane:Garth:Heather,
+    Garth - Beverly:Diane:Ed:Fernando:Heather,
+    Heather - Fernando:Garth:Ike,
+    Ike - Heather:Jane,
+    Jane - Ike
+  )
+
+  c1 <- closeness(kite, normalized = TRUE)
+  c2 <- centr_clo(kite)
+  expect_equal(unname(c1), c2$res)
+  expect_equal(c2$centralization, 0.270374931581828)
+  expect_equal(c2$theoretical_max, 4.23529411764706)
+})
