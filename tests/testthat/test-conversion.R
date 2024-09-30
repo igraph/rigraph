fbc12ddf8f94b9258d407f7164fd4b4245acff77
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-30 15:38:31 +0200

    test: fold `as_adj_list()`'s test into test-conversion

--- tests/testthat/test-conversion.R
+++ tests/testthat/test-conversion.R
@@ -262,3 +262,38 @@ test_that("as_edgelist() works", {
   g2 <- make_graph(t(e), n = vcount(g), dir = FALSE)
   expect_isomorphic(g, g2)
 })
+
+test_that("as_adj_list() works", {
+  g <- sample_gnp(50, 2 / 50)
+  al <- as_adj_list(g)
+  g2 <- graph_from_adj_list(al, mode = "all")
+  expect_isomorphic(g, g2)
+  expect_true(isomorphic(g, g2,
+    vertex.color1 = 1:vcount(g),
+    vertex.color2 = 1:vcount(g2),
+    method = "vf2"
+  ))
+
+  ####
+
+  el <- as_adj_edge_list(g)
+  for (i in 1:vcount(g)) {
+    a <- E(g)[.inc(i)]
+    expect_equal(length(a), length(el[[i]]), ignore_attr = TRUE)
+    expect_equal(sort(el[[i]]), sort(a), ignore_attr = TRUE)
+  }
+
+  g <- sample_gnp(50, 4 / 50, directed = TRUE)
+  el1 <- as_adj_edge_list(g, mode = "out")
+  el2 <- as_adj_edge_list(g, mode = "in")
+  for (i in 1:vcount(g)) {
+    a <- E(g)[.from(i)]
+    expect_equal(length(a), length(el1[[i]]), ignore_attr = TRUE)
+    expect_equal(sort(el1[[i]]), sort(a), ignore_attr = TRUE)
+  }
+  for (i in 1:vcount(g)) {
+    a <- E(g)[.to(i)]
+    expect_equal(length(a), length(el2[[i]]), ignore_attr = TRUE)
+    expect_equal(sort(el2[[i]]), sort(a), ignore_attr = TRUE)
+  }
+})
