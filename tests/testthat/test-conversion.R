92654f232b75962b5d05e44ddcfb8398e68b1d9f
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-30 15:29:58 +0200

    test: fold as_edgelist()'s test into test-conversion

--- tests/testthat/test-conversion.R
+++ tests/testthat/test-conversion.R
@@ -255,3 +255,10 @@ test_that("as_adjacency_matrix() works -- dense + not both", {
     )
   )
 })
+
+test_that("as_edgelist() works", {
+  g <- sample_gnp(100, 3 / 100)
+  e <- as_edgelist(g)
+  g2 <- make_graph(t(e), n = vcount(g), dir = FALSE)
+  expect_isomorphic(g, g2)
+})
