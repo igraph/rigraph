7c75179ea29624964d26b0445e3dc533b1061439
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-10-01 10:13:28 +0200

    test: fold `ivs()`'s tests into test-cliques

--- tests/testthat/test-cliques.R
+++ tests/testthat/test-cliques.R
@@ -217,3 +217,12 @@ test_that("count_max_cliques works", {
 
   expect_identical(cl1, cl2)
 })
+
+test_that("ivs() works", {
+  g <- sample_gnp(50, 0.8)
+  ivs <- ivs(g, min = ivs_size(g))
+  ec <- sapply(seq_along(ivs), function(x) {
+    ecount(induced_subgraph(g, ivs[[x]]))
+  })
+  expect_equal(unique(ec), 0)
+})
