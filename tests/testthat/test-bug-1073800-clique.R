79c8d2c0039e5186de80423ea4fa54235b709f15
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-10-01 11:54:35 +0200

    test: fold `largest_cliques()`'s test into test-cliques

--- tests/testthat/test-bug-1073800-clique.R
+++
@@ -1,9 +0,0 @@
-test_that("largest_cliques() works", {
-  adj <- matrix(1, nrow = 11, ncol = 11) - diag(11)
-  g <- graph_from_adjacency_matrix(adj)
-  expect_warning(
-    lc <- largest_cliques(g),
-    "Edge directions are ignored for maximal clique calculation"
-  )
-  expect_equal(lapply(unvs(lc), sort), list(1:11))
-})
--- tests/testthat/test-cliques.R
+++ tests/testthat/test-cliques.R
@@ -226,3 +226,13 @@ test_that("ivs() works", {
   })
   expect_equal(unique(ec), 0)
 })
+
+test_that("largest_cliques() works", {
+  adj <- matrix(1, nrow = 11, ncol = 11) - diag(11)
+  g <- graph_from_adjacency_matrix(adj)
+  expect_warning(
+    lc <- largest_cliques(g),
+    "Edge directions are ignored for maximal clique calculation"
+  )
+  expect_equal(lapply(unvs(lc), sort), list(1:11))
+})
