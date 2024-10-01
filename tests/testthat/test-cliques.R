6feb787e925dcbb753da52c946cb71d10a52fc14
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-10-01 11:55:52 +0200

    test: fold `largest_ivs()`'s test into test-clique

--- tests/testthat/test-cliques.R
+++ tests/testthat/test-cliques.R
@@ -236,3 +236,19 @@ test_that("largest_cliques() works", {
   )
   expect_equal(lapply(unvs(lc), sort), list(1:11))
 })
+
+test_that("largest_ivs() works", {
+  g <- sample_gnp(50, 0.8)
+  livs <- largest_ivs(g)
+  expect_equal(
+    unique(sapply(livs, length)),
+    ivs_size(g)
+  )
+
+  ec <- sapply(seq_along(livs), function(x) {
+    ecount(induced_subgraph(g, livs[[x]]))
+  })
+  expect_equal(unique(ec), 0)
+
+  ## TODO: check that they are largest
+})
