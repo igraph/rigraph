239fce481ca2a57d17632c6bbe844c356141cd9d
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-10-01 11:53:59 +0200

    test: slight refactoring of `largest_cliques()`'s test

--- tests/testthat/test-bug-1073800-clique.R
+++ tests/testthat/test-bug-1073800-clique.R
@@ -1,6 +1,9 @@
-test_that("Largest cliques is correct", {
+test_that("largest_cliques() works", {
   adj <- matrix(1, nrow = 11, ncol = 11) - diag(11)
   g <- graph_from_adjacency_matrix(adj)
-  lc <- suppressWarnings(largest_cliques(g))
+  expect_warning(
+    lc <- largest_cliques(g),
+    "Edge directions are ignored for maximal clique calculation"
+  )
   expect_equal(lapply(unvs(lc), sort), list(1:11))
 })
