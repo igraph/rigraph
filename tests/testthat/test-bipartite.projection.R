68caeb32e0835d596c41dc701133b331b9b84780
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-12-10 15:01:27 +0100

    feat: Split `sample_bipartite()` into two functions for the G(n,m) and G(n,p) case
    docs: fix manual page
    
    docs: fix for pkgdown index
    
    chore: also deprecate `bipartite()`
    
    fix: fix deprecation message
    
    docs: run `document()`
    
    test: update snap
    
    docs: fix manual page for `bipartite_gnp()`

--- tests/testthat/test-bipartite.projection.R
+++ tests/testthat/test-bipartite.projection.R
@@ -40,7 +40,7 @@ test_that("bipartite_projection works", {
 test_that("bipartite_projection can calculate only one projection", {
   withr::local_seed(42)
 
-  g <- sample_bipartite(5, 10, p = .3)
+  g <- sample_bipartite_gnp(5, 10, p = .3)
   proj <- bipartite_projection(g)
   proj1 <- bipartite_projection(g, which = "false")
   proj2 <- bipartite_projection(g, which = "true")
