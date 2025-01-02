932de61bcf4392a3e08d6256c95c7c3c95b3b3ae
Kirill Müller <kirill@cynkra.com>
2025-01-02 16:56:55 +0100

    vendor: Update vendored sources to igraph/igraph@1d6ef7c417e38c881ef325d8c553ccf767db6b6f
    refactor: minor readability cleanup in igraph_decompose()

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.13-133-g0878f577d"
+#define IGRAPH_VERSION "0.10.13-134-g1d6ef7c41"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 13
-#define IGRAPH_VERSION_PRERELEASE "133-g0878f577d"
+#define IGRAPH_VERSION_PRERELEASE "134-g1d6ef7c41"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
