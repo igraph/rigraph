cb9f56a1ae601a0e315f28ced348263eea2bbaa6
Kirill Müller <kirill@cynkra.com>
2025-01-02 16:58:47 +0100

    vendor: Update vendored sources to igraph/igraph@5cd6552bf00d6a774cdd64b5fabf2c79524ccfc7
    fix: add some missing IGRAPH_CHECKs

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.13-137-g82d35eebd"
+#define IGRAPH_VERSION "0.10.13-138-g5cd6552bf"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 13
-#define IGRAPH_VERSION_PRERELEASE "137-g82d35eebd"
+#define IGRAPH_VERSION_PRERELEASE "138-g5cd6552bf"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
