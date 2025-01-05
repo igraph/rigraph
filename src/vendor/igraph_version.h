2bac7619bfd2e1d10d5f4d3caba756bfbfe8207f
Kirill Müller <kirill@cynkra.com>
2025-01-05 22:36:13 +0100

    vendor: Update vendored sources to igraph/igraph@cd7bcd9d9b9bfd9acd83dace6438507947297ce2
    refactor: cleaner parameter ordering in igraph_i_simple_cycles_circuit()

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.13-157-gdafe829b8"
+#define IGRAPH_VERSION "0.10.13-158-gcd7bcd9d9"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 13
-#define IGRAPH_VERSION_PRERELEASE "157-gdafe829b8"
+#define IGRAPH_VERSION_PRERELEASE "158-gcd7bcd9d9"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
