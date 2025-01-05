9947635a9c78f62da6863d5cbfee2504210234d9
Kirill Müller <kirill@cynkra.com>
2025-01-05 21:26:24 +0100

    vendor: Update vendored sources to igraph/igraph@3b9cb6ff078cf4e7fdb2be593811e50b97232fea
    interface: add more missing OPTIONAL markers

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.13-148-g5512e864e"
+#define IGRAPH_VERSION "0.10.13-149-g3b9cb6ff0"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 13
-#define IGRAPH_VERSION_PRERELEASE "148-g5512e864e"
+#define IGRAPH_VERSION_PRERELEASE "149-g3b9cb6ff0"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
