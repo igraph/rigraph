c737da97c3ba3c86ad8871753ccc0449920b047c
Kirill Müller <kirill@cynkra.com>
2025-01-05 22:41:46 +0100

    vendor: Update vendored sources to igraph/igraph@3631b1d5a0f3b7e49463f89cbbba762b24d9383d
    refactor: clean up igraph_path_length_hist()

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.15-12-g6fd3903c8"
+#define IGRAPH_VERSION "0.10.15-13-g3631b1d5a"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 15
-#define IGRAPH_VERSION_PRERELEASE "12-g6fd3903c8"
+#define IGRAPH_VERSION_PRERELEASE "13-g3631b1d5a"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
