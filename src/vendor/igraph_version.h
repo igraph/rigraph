0e9c03a2b97af9746d272d60b727ef6ab11a2de8
Kirill Müller <kirill@cynkra.com>
2025-01-05 22:42:53 +0100

    vendor: Update vendored sources to igraph/igraph@5e734a2c1211d017088a882ed121f25956e20189
    ci: support for controlling the use of plfit and improve alpine jon

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.15-16-ged43183a2"
+#define IGRAPH_VERSION "0.10.15-17-g5e734a2c1"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 15
-#define IGRAPH_VERSION_PRERELEASE "16-ged43183a2"
+#define IGRAPH_VERSION_PRERELEASE "17-g5e734a2c1"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
