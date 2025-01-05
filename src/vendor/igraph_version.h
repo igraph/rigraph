ca8c32b76e23864c8cc0d9e04bc8bd8802d65e16
Kirill Müller <kirill@cynkra.com>
2025-01-05 22:37:05 +0100

    vendor: Update vendored sources to igraph/igraph@12299c4928f9f463c06d0b46752df69a6d67c289
    fix: temporary fix for simple cycle search mishandling self-loops, ref igraph/igraph#2692
    test: minimum cycle length for igraph_simple_cycles()

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.13-159-g26d3718b3"
+#define IGRAPH_VERSION "0.10.13-161-g12299c492"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 13
-#define IGRAPH_VERSION_PRERELEASE "159-g26d3718b3"
+#define IGRAPH_VERSION_PRERELEASE "161-g12299c492"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
