49cf698571f1f4e0e18fab94fce1bde28c58c6dd
Kirill Müller <kirill@cynkra.com>
2025-01-05 22:31:09 +0100

    vendor: Update vendored sources to igraph/igraph@797ab4fa6c9a119ff12af1f5d6080de8d1e4f142
    interface: mark all callback extra parameters and some callback functions as optional

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.13-151-g518fcc416"
+#define IGRAPH_VERSION "0.10.13-152-g797ab4fa6"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 13
-#define IGRAPH_VERSION_PRERELEASE "151-g518fcc416"
+#define IGRAPH_VERSION_PRERELEASE "152-g797ab4fa6"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
