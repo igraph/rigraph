cc2932b0d0cf237e339b6f0c916586c996f4708c
Kirill Müller <kirill@cynkra.com>
2025-01-06 05:21:50 +0100

    vendor: Update vendored sources to igraph/igraph@befc831fff74e4bae39743b1f83b38e6b3af0390
    fix: correct some implicit integer conversions

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.15-21-g5e3c56c6a"
+#define IGRAPH_VERSION "0.10.15-22-gbefc831ff"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 15
-#define IGRAPH_VERSION_PRERELEASE "21-g5e3c56c6a"
+#define IGRAPH_VERSION_PRERELEASE "22-gbefc831ff"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
