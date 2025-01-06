2bdeb05fbf3fac3e01b730236dee57a2ee6cc797
Kirill Müller <kirill@cynkra.com>
2025-01-06 05:23:22 +0100

    vendor: Update vendored sources to igraph/igraph@082e2c6ba1a96d34d05a4e75a59c4f690aeca58d
    chore: update changelog [skip ci]

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.15-28-g8d242741a"
+#define IGRAPH_VERSION "0.10.15-29-g082e2c6ba"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 15
-#define IGRAPH_VERSION_PRERELEASE "28-g8d242741a"
+#define IGRAPH_VERSION_PRERELEASE "29-g082e2c6ba"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
