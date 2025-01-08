b7104efbab55cc3ad1e0eda4cc1b27da1b31ac3f
Kirill Müller <kirill@cynkra.com>
2025-01-08 05:52:17 +0100

    vendor: Update vendored sources to igraph/igraph@e0dbe374e30adc654ad55a50d5aaf47ebc753908
    chore: update plfit to 1.0.0

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.15-32-g2cdc20a37"
+#define IGRAPH_VERSION "0.10.15-33-ge0dbe374e"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 15
-#define IGRAPH_VERSION_PRERELEASE "32-g2cdc20a37"
+#define IGRAPH_VERSION_PRERELEASE "33-ge0dbe374e"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
