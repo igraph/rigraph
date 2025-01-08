094796d5d1060c0ae0292382ef82e8f04c49f8ed
Kirill Müller <kirill@cynkra.com>
2025-01-08 05:54:29 +0100

    vendor: Update vendored sources to igraph/igraph@5216243786a5368ab14db9649008d1caca75f5af
    refactor: cleaner workaround in plfit for Windows 11 SDK bug where NAN is defined in a non-constant manner

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.15-46-g622f331dc"
+#define IGRAPH_VERSION "0.10.15-47-g521624378"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 15
-#define IGRAPH_VERSION_PRERELEASE "46-g622f331dc"
+#define IGRAPH_VERSION_PRERELEASE "47-g521624378"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
