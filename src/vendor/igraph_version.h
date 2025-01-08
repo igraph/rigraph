3acfad9f859ef53b7f2d6932ea77683256cb642e
Kirill Müller <kirill@cynkra.com>
2025-01-08 05:53:18 +0100

    vendor: Update vendored sources to igraph/igraph@c675c805a6ed436887ec0e6f9c6e18ca6d13ecad
    chore: update CodeCoverage.cmake from upstream https://github.com/bilke/cmake-modules/blob/master/CodeCoverage.cmake

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.15-43-gb25eda2db"
+#define IGRAPH_VERSION "0.10.15-44-gc675c805a"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 15
-#define IGRAPH_VERSION_PRERELEASE "43-gb25eda2db"
+#define IGRAPH_VERSION_PRERELEASE "44-gc675c805a"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
