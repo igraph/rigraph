a1eb6e44bea97399d785e857985b2355f32707b2
krlmlr <krlmlr@users.noreply.github.com>
2025-02-10 13:37:01 +0000

    vendor: Update vendored sources to igraph/igraph@199fdbf0a511294c7444eaa25c1861e784b3aa8a
    refactor: igraph_lastcit_game() cleanup

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.15-50-g27624a9b6"
+#define IGRAPH_VERSION "0.10.15-51-g199fdbf0a"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 15
-#define IGRAPH_VERSION_PRERELEASE "50-g27624a9b6"
+#define IGRAPH_VERSION_PRERELEASE "51-g199fdbf0a"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
