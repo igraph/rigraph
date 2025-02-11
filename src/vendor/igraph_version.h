396d7f79a53d122f628ebb11c22835cb3ccc10dc
krlmlr <krlmlr@users.noreply.github.com>
2025-02-11 18:59:26 +0000

    vendor: Update vendored sources to igraph/igraph@6981deb377ddd9ca9f098d683a743c3dd793e563
    refactor: various readability improvements

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.15-52-g7955e08f3"
+#define IGRAPH_VERSION "0.10.15-53-g6981deb37"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 15
-#define IGRAPH_VERSION_PRERELEASE "52-g7955e08f3"
+#define IGRAPH_VERSION_PRERELEASE "53-g6981deb37"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
