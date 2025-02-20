7f8ca0e68e111ca4350118e9e26f416398397642
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-02-20 10:26:11 +0100

    test: improve test-dot.product.game

--- tests/testthat/test-dot.product.game.R
+++ tests/testthat/test-dot.product.game.R
@@ -9,10 +9,6 @@ test_that("Dot product rng works", {
   g0 <- graph_from_literal(1:2:3 - 4)
   expect_equal(as.matrix(g[]), as.matrix(g0[]), ignore_attr = TRUE)
 
-  g2 <- sample_dot_product(vecs, directed = TRUE)
-  g20 <- graph_from_literal(1:2:3:4, 1 -+ 3, 1 -+ 4, 3 -+ 4, 4 +- 1, 4 +- 3)
-  expect_true(all.equal(g20[], g[], check.attributes = FALSE))
-
   vecs <- replicate(5, rep(1 / 2, 4))
   g <- sample_dot_product(vecs)
   expect_equal(g[], make_full_graph(5)[], ignore_attr = TRUE)
@@ -22,10 +18,32 @@ test_that("Dot product rng works", {
 
   vecs <- replicate(100, rep(sqrt(1 / 8), 4))
   g <- sample_dot_product(vecs)
-  expect_ecount(g, 2454)
+  expect_ecount(g, 2451)
 
   g2 <- sample_dot_product(vecs, directed = TRUE)
-  expect_ecount(g2, 4938)
+  expect_ecount(g2, 4941)
+})
+
+test_that("MISSING TITLE", {
+  withr::local_seed(42)
+  latent_features <- cbind(
+    c(0, 1, 1, 1, 0) / 3, c(0, 1, 1, 0, 1) / 3, c(1, 1, 1, 1, 0) / 4,
+    c(0, 1, 1, 1, 0)
+  )
+  expected_probs <- t(latent_features)%*%latent_features
+  diag(expected_probs) <- 0
+  num_graphs <- 1000
+  edge_counts <- matrix(0, nrow = 4, ncol = 4)
+
+  for (i in seq_len(num_graphs)) {
+    g <- sample_dot_product(latent_features)
+    adj_matrix <- as_adjacency_matrix(g, sparse = FALSE)
+    edge_counts <- edge_counts + adj_matrix
+  }
+  empirical_probs <- edge_counts / num_graphs
+  diag(empirical_probs) <- 0
+  tolerance <- 0.05
+  expect_true(all(abs(empirical_probs - expected_probs) < tolerance))
 })
 
 test_that("Dot product rng gives warnings", {
