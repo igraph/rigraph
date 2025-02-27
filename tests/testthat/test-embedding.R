3a1c3f6342cbf2e93348a085095ad3d9c2533864
schochastics <david@schochastics.net>
2025-02-27 14:23:53 +0100

    added dimselect

--- tests/testthat/test-embedding.R
+++ tests/testthat/test-embedding.R
@@ -838,3 +838,33 @@ test_that("Sampling sphere volume works", {
   expect_true(all(sqrt(colSums(s3^2)) < 1 / 2))
   expect_true(all(s3 >= 0))
 })
+
+test_that("dimensionality selection works", {
+  withr::local_seed(42)
+
+  karate <- make_graph("zachary")
+  ev <- eigen(as_adjacency_matrix(karate), only.values = TRUE)$values
+  kdim <- dim_select(ev)
+  expect_equal(kdim, 4)
+
+  expect_equal(dim_select(1:100), 50)
+
+  ## Some regression tests
+  expect_equal(dim_select(runif(100)), 69)
+  expect_equal(dim_select(runif(100)), 88)
+  expect_equal(dim_select(runif(100)), 3)
+  expect_equal(dim_select(runif(100)), 99)
+
+  ## Some more meaningful tests
+  x <- c(rnorm(50, mean = 0, sd = 1), rnorm(50, mean = 5, sd = 1))
+  expect_equal(dim_select(x), 50)
+
+  x <- c(rnorm(10, mean = 0, sd = 1), rnorm(90, mean = 2, sd = 1))
+  expect_equal(dim_select(x), 10)
+
+  x <- c(10, rnorm(99, mean = 0, sd = 1))
+  expect_equal(dim_select(x), 1)
+
+  x <- c(rnorm(99, mean = 0, sd = 1), 10)
+  expect_equal(dim_select(x), 99)
+})
