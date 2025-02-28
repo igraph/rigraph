b18d1f9442fd2ee81caab3aeeeaa9d6552b665c3
David Schoch <david.schoch@cynkra.com>
2025-02-28 05:52:32 +0100

    test: merged and refactored structural.properties.R tests (#1726)

--- tests/testthat/test-reciprocity.R
+++
@@ -1,5 +0,0 @@
-test_that("reciprocity works", {
-  g <- make_graph(c(1, 2, 2, 1, 2, 3, 3, 4, 4, 4), directed = TRUE)
-  expect_equal(reciprocity(g), 0.5)
-  expect_equal(reciprocity(g, ignore.loops = FALSE), 0.6)
-})
