3c946ccdb05883080df5e76eae4664c5a0307d37
krlmlr <krlmlr@users.noreply.github.com>
2025-03-22 01:09:32 +0000

    vendor: Update vendored sources to igraph/igraph@dd208201b117baecfb1f0de9018e20a6929da865
    feat: igraph_rng_get_bool()

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.15-80-g64e464903"
+#define IGRAPH_VERSION "0.10.15-81-gdd208201b"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 15
-#define IGRAPH_VERSION_PRERELEASE "80-g64e464903"
+#define IGRAPH_VERSION_PRERELEASE "81-gdd208201b"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
