7101b51ef259a78cc18798600943866c0caaeb05
David Schoch <david.schoch@cynkra.com>
2025-04-04 22:44:30 +0200

    test: added more weight tests to `graph_from_biadjacency_matrix()` (#1804)

--- tests/testthat/test-incidence.R
+++ tests/testthat/test-incidence.R
@@ -73,6 +73,12 @@ test_that("graph_from_biadjacency_matrix() works - dense, modes, weighted", {
   expect_true(is_directed(mutual_g))
   expect_length(E(mutual_g), 16)
   expect_equal(as_adj_list(mutual_g, mode = "all")$A %>% as.numeric(), c(6, 6, 7, 7, 8, 8))
+
+  inc_frac <- matrix(runif(15), 3, 5)
+  colnames(inc_frac) <- letters[1:5]
+  rownames(inc_frac) <- LETTERS[1:3]
+  frac_g <- graph_from_biadjacency_matrix(inc_frac, directed = TRUE, mode = "out", weighted = TRUE)
+  expect_equal(inc_frac, as_biadjacency_matrix(frac_g, attr = "weight"))
 })
 
 test_that("graph_from_biadjacency_matrix() works -- sparse", {
@@ -133,25 +139,25 @@ test_that("graph_from_biadjacency_matrix() works - sparse, modes, weighted", {
   local_igraph_options(print.id = FALSE)
   withr::local_seed(42)
 
-  inc <- matrix(sample(0:1, 15, repl = TRUE), 3, 5)
+  inc <- matrix(sample(0:2, 15, repl = TRUE), 3, 5)
   inc <- Matrix::Matrix(inc, sparse = TRUE)
   colnames(inc) <- letters[1:5]
   rownames(inc) <- LETTERS[1:3]
 
   out_g <- graph_from_biadjacency_matrix(inc, directed = TRUE, mode = "out", weighted = TRUE)
   expect_true(is_directed(out_g))
-  expect_length(E(out_g), 7)
-  expect_equal(as_adj_list(out_g, mode = "out")$A %>% as.numeric(), c(6, 7))
+  expect_length(E(out_g), 8)
+  expect_equal(as_adj_list(out_g, mode = "out")$A %>% as.numeric(), c(6, 7, 8))
 
   in_g <- graph_from_biadjacency_matrix(inc, directed = TRUE, mode = "in", weighted = TRUE)
   expect_true(is_directed(in_g))
-  expect_length(E(in_g), 7)
-  expect_equal(as_adj_list(in_g, mode = "in")$A %>% as.numeric(), c(6, 7))
+  expect_length(E(in_g), 8)
+  expect_equal(as_adj_list(in_g, mode = "in")$A %>% as.numeric(), c(6, 7, 8))
 
   mutual_g <- graph_from_biadjacency_matrix(inc, directed = TRUE, mode = "all", weighted = TRUE)
   expect_true(is_directed(mutual_g))
-  expect_length(E(mutual_g), 14)
-  expect_equal(as_adj_list(mutual_g, mode = "all")$A %>% as.numeric(), c(6, 6, 7, 7))
+  expect_length(E(mutual_g), 16)
+  expect_equal(as_adj_list(mutual_g, mode = "all")$A %>% as.numeric(), c(6, 6, 7, 7, 8, 8))
 })
 
 test_that("graph_from_biadjacency_matrix() errors well", {
