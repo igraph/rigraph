c833695a934668202c1d0b12746ec2145f6fb72c
krlmlr <krlmlr@users.noreply.github.com>
2025-04-07 13:06:23 +0000

    vendor: Update vendored sources to igraph/igraph@d48a25ba0f6d72e447c6d17d74b667b1f21754fb
    docs: add stable links to ARG Database in graphdb reader docs

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.15-105-ga68d7cafc"
+#define IGRAPH_VERSION "0.10.15-106-gd48a25ba0"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 15
-#define IGRAPH_VERSION_PRERELEASE "105-ga68d7cafc"
+#define IGRAPH_VERSION_PRERELEASE "106-gd48a25ba0"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
