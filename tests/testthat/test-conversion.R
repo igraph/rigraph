dc3aae0d3bcc599454b0c0cc98aef7109959dbc6
David Schoch <david.schoch@cynkra.com>
2025-04-29 20:09:50 +0200

    fix: added factor support for `graph_from_data_frame()` (#1829)

--- tests/testthat/test-conversion.R
+++ tests/testthat/test-conversion.R
@@ -596,3 +596,26 @@ test_that("edge names work", {
     structure(c("b", "c", "d", "e", "g", "h", "a", "c", "d", "e", "f", "h", "i", "j"), .Dim = c(7L, 2L))
   )
 })
+
+test_that("graph_from_data_frame works with factors", {
+  actors <- data.frame(
+    name = c("Alice", "Bob", "Cecil", "David", "Esmeralda"),
+    age = c(48, 33, 45, 34, 21),
+    gender = factor(c("F", "M", "F", "M", "F"))
+  )
+  relations <- data.frame(
+    from = c(
+      "Bob", "Cecil", "Cecil", "David",
+      "David", "Esmeralda"
+    ),
+    to = c("Alice", "Bob", "Alice", "Alice", "Bob", "Alice"),
+    same.dept = c(FALSE, FALSE, TRUE, FALSE, FALSE, TRUE),
+    friendship = c(4, 5, 5, 2, 1, 1), advice = c(4, 5, 5, 4, 2, 3)
+  )
+  g <- graph_from_data_frame(relations, directed = TRUE, vertices = actors)
+
+  g_actors <- as_data_frame(g, what = "vertices")
+
+  expect_true(is.factor(V(g)$gender))
+  expect_true(is.factor(g_actors$gender))
+})
