25afb4d1949c6772ca4b74fcadcad85f59b13f04
schochastics <david@schochastics.net>
2025-06-25 13:10:56 +0200

    fixed layout for test graph

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -151,8 +151,9 @@ test_that("Multi loops are arranged correctly", {
   vdiffr::expect_doppelganger("multi-loops-triangle", multi_loops_triangle)
 
   multi_loops_many <- function() {
-    withr::local_seed(42)
     g2 <- make_graph(c(1,2, 2,3, 3,1, 1,4, 4,5, 5,1, 3,4, 5,2, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1),directed = FALSE)
+    V(g2)$x <- c(1,2,2,0,0)
+    V(g2)$y <- c(1,0,2,2,0)
     plot(g2, loop.size = 2)
   }
 
