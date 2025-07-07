01b26a713fb3c2c9bc675bfadfc3d8366aefbe4d
schochastics <david@schochastics.net>
2025-07-07 20:31:58 +0200

    added test

--- tests/testthat/test-attributes.R
+++ tests/testthat/test-attributes.R
@@ -454,9 +454,13 @@ test_that("empty returns work", {
 
 test_that("assign data.frame attributes works", {
   # https://github.com/igraph/rigraph/issues/1669
-  g <- make_tree(10, 3)
-  edge.attributes(g) <- head(mtcars, ecount(g))
-  expect_no_error(E(g)[c(1, 2)])
+  g1 <- make_tree(10, 3)
+  edge.attributes(g1) <- head(mtcars, ecount(g1))
+  expect_no_error(E(g1)[c(1, 2)])
+
+  g2 <- make_tree(10, 3)
+  edge.attributes(g2, E(g2)[1:5]) <- head(mtcars, 5)
+  expect_equal(E(g2)$wt, c(mtcars$wt[1:5], rep(NA, ecount(g2) - 5)))
 })
 
 test_that("good error message when not using character", {
