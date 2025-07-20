4dd90b7c65adcedd1009427d13079281a9c9ff3b
krlmlr <krlmlr@users.noreply.github.com>
2025-07-20 09:26:35 +0000

    vendor: Update vendored sources to igraph/igraph@0224bb3745b974fa7b6804af4ef893e85e7e1f8c
    docs: fix additional consistency issues between docs and declarations

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.16-58-gf73277f19"
+#define IGRAPH_VERSION "0.10.16-59-g0224bb374"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 16
-#define IGRAPH_VERSION_PRERELEASE "58-gf73277f19"
+#define IGRAPH_VERSION_PRERELEASE "59-g0224bb374"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
