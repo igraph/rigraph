3a6222eedb7ff32ce470bcd820bd623607f96eaf
krlmlr <krlmlr@users.noreply.github.com>
2025-08-17 20:13:29 +0000

    vendor: Update vendored sources to igraph/igraph@378629ea57ee631844c391237faa021d8652cbf4
    chore: standardize spelling in igraph_product() docs [skip ci]

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.16-105-g5819c1bcb"
+#define IGRAPH_VERSION "0.10.16-106-g378629ea5"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 16
-#define IGRAPH_VERSION_PRERELEASE "105-g5819c1bcb"
+#define IGRAPH_VERSION_PRERELEASE "106-g378629ea5"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
