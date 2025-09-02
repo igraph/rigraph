8c1d6b993c0c09b444042e10efb40b1f2dafdb20
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-09-02 11:21:25 +0200

    rm extra impl

--- src/rinterface_extra.c
+++ src/rinterface_extra.c
@@ -3893,7 +3893,6 @@ SEXP R_igraph_neighbors(SEXP graph, SEXP pvid, SEXP pmode) {
   return result;
 }
 
-SEXP R_igraph_incident(SEXP graph, SEXP pvid, SEXP pmode) {
 
   igraph_t g;
   igraph_vector_int_t neis;
