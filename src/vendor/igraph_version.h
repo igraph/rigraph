8f7572066dd41ae2fed8dcf3e7df365507ee08a7
krlmlr <krlmlr@users.noreply.github.com>
2025-09-14 12:19:58 +0000

    vendor: Update vendored sources to igraph/igraph@90dc1fc593cef32a7d836f7b0d1908050cdc3ab7
    ci: use Clang 22

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.10.16-154-g0630ac8e5"
+#define IGRAPH_VERSION "0.10.16-155-g90dc1fc59"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 10
 #define IGRAPH_VERSION_PATCH 16
-#define IGRAPH_VERSION_PRERELEASE "154-g0630ac8e5"
+#define IGRAPH_VERSION_PRERELEASE "155-g90dc1fc59"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
