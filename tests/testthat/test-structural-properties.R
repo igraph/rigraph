ede7b4bdeb3db1654c3f8a8f8caa1e2ffd5fb6fa
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-10-09 13:15:14 +0200

    test: split knn tests (#2199)

--- tests/testthat/test-structural-properties.R
+++ tests/testthat/test-structural-properties.R
@@ -905,7 +905,7 @@ test_that("edge_density works", {
   expect_equal(gd, gd2)
 })
 
-test_that("knn works", {
+test_that("knn works -- trivial examples", {
   withr::local_seed(42)
 
   ## Some trivial ones
@@ -917,23 +917,28 @@ test_that("knn works", {
     knn(g2),
     list(knn = c(1, rep(9, 9)), knnk = c(9, rep(NaN, 7), 1))
   )
+})
 
-  ## A scale-free one, try to plot 'knnk'
+test_that("knn works -- scale-free one", {
+  withr::local_seed(42)
   g3 <- simplify(sample_pa(1000, m = 5))
   r3 <- knn(g3)
   expect_equal(r3$knn[43], 46)
   expect_equal(r3$knn[1000], 192.4)
   expect_equal(r3$knnk[100], 18.78)
   expect_equal(length(r3$knnk), 359)
+})
 
-  ## A random graph
+test_that("knn works -- random graph", {
+  withr::local_seed(42)
   g4 <- sample_gnp(1000, p = 5 / 1000)
   r4 <- knn(g4)
-  expect_equal(r4$knn[1000], 20 / 3)
+  expect_equal(r4$knn[1000], 5)
   expect_equal(length(r4$knnk), 15)
-  expect_equal(r4$knnk[12], 19 / 3)
+  expect_equal(r4$knnk[12], 5.6389, tolerance = 1e-5)
+})
 
-  ## A weighted graph
+test_that("knn works -- weighted graph", {
   g5 <- make_star(10)
   E(g5)$weight <- seq(ecount(g5))
   r5 <- knn(g5)
