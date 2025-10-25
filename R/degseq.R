f037f632d62febe2fc8fd0a88a42ae1002d89879
krlmlr <krlmlr@users.noreply.github.com>
2025-10-25 22:20:41 +0000

    chore: Auto-update from GitHub Actions
    Run: https://github.com/igraph/rigraph/actions/runs/18808836867

--- R/degseq.R
+++ R/degseq.R
@@ -140,7 +140,11 @@ is_degseq <- function(out.deg, in.deg = NULL) {
 #' is_graphical(degree(g))
 #' @export
 #' @cdocs igraph_is_graphical
-is_graphical <- function(out.deg, in.deg = NULL, allowed.edge.types = c("simple", "loops", "multi", "all")) {
+is_graphical <- function(
+  out.deg,
+  in.deg = NULL,
+  allowed.edge.types = c("simple", "loops", "multi", "all")
+) {
   is_graphical_impl(
     out.deg = out.deg,
     in.deg = in.deg,
