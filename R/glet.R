6c8b949b892893de2884b8f4a5974c631fc9042c
Kirill Müller <kirill@cynkra.com>
2025-11-02 12:42:14 +0100

    g1, change in behavior

--- R/glet.R
+++ R/glet.R
@@ -136,22 +136,10 @@ graphlets.candidate.basis <- function(graph, weights = NULL) {
 #' @family glet
 #' @export
 graphlet_basis <- function(graph, weights = NULL) {
-  ## Argument checks
-  ensure_igraph(graph)
-  if (is.null(weights) && "weight" %in% edge_attr_names(graph)) {
-    weights <- E(graph)$weight
-  }
-  if (!is.null(weights) && any(!is.na(weights))) {
-    weights <- as.numeric(weights)
-  } else {
-    weights <- NULL
-  }
-
-  on.exit(.Call(R_igraph_finalizer))
-  ## Function call
-  res <- .Call(Rx_igraph_graphlets_candidate_basis, graph, weights)
-
-  res
+  graphlets_candidate_basis_impl(
+    graph = graph,
+    weights = weights
+  )
 }
 
 #' @rdname graphlet_basis
