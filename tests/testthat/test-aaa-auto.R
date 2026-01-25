a33fa7e0e76550d6265579e9e146ab966e1b7ee4
Kirill Müller <kirill@cynkra.com>
2026-01-25 19:15:21 +0100

    Move

--- tests/testthat/test-aaa-auto.R
+++ tests/testthat/test-aaa-auto.R
@@ -11138,6 +11138,121 @@ test_that("independent_vertex_sets_impl basic", {
 
 # Callback functions
 
+# bfs_closure_impl
+
+test_that("bfs_closure_impl works", {
+  withr::local_seed(20250125)
+  local_igraph_options(print.id = FALSE)
+
+  g <- make_ring(10)
+
+  # Collect BFS visit data
+  bfs_visits <- list()
+  result <- bfs_closure_impl(
+    graph = g,
+    root = 1,
+    mode = "out",
+    unreachable = TRUE,
+    restricted = NULL,
+    callback = function(args) {
+      bfs_visits[[length(bfs_visits) + 1]] <<- args
+      TRUE # Continue
+    }
+  )
+
+  expect_snapshot({
+    cat("BFS result:\n")
+    print(result)
+    cat("\nNumber of BFS visits:", length(bfs_visits), "\n")
+    if (length(bfs_visits) > 0) {
+      cat("First visit:\n")
+      print(bfs_visits[[1]])
+    }
+  })
+
+  expect_snapshot_igraph_error({
+    bfs_closure_impl(
+      graph = g,
+      root = 1,
+      mode = "out",
+      unreachable = TRUE,
+      restricted = NULL,
+      callback = function(args) {
+        NA
+      }
+    )
+  })
+
+  expect_snapshot_igraph_error({
+    bfs_closure_impl(
+      graph = g,
+      root = 1,
+      mode = "out",
+      unreachable = TRUE,
+      restricted = NULL,
+      callback = function(args) {
+        NA
+      }
+    )
+  })
+
+  calls <- 0
+  bfs_closure_impl(
+    graph = g,
+    root = 1,
+    mode = "out",
+    unreachable = TRUE,
+    restricted = NULL,
+    callback = function(args) {
+      calls <<- calls + 1
+      calls <= 3
+    }
+  )
+  expect_equal(calls, 3)
+})
+
+# dfs_closure_impl
+
+test_that("dfs_closure_impl works", {
+  withr::local_seed(20250125)
+  local_igraph_options(print.id = FALSE)
+
+  g <- make_ring(10)
+
+  # Collect DFS visit data
+  dfs_in_visits <- list()
+  dfs_out_visits <- list()
+  result <- dfs_closure_impl(
+    graph = g,
+    root = 1,
+    mode = "out",
+    unreachable = TRUE,
+    in_callback = function(args) {
+      dfs_in_visits[[length(dfs_in_visits) + 1]] <<- args
+      TRUE # Continue
+    },
+    out_callback = function(args) {
+      dfs_out_visits[[length(dfs_out_visits) + 1]] <<- args
+      TRUE # Continue
+    }
+  )
+
+  expect_snapshot({
+    cat("DFS result:\n")
+    print(result)
+    cat("\nNumber of DFS IN visits:", length(dfs_in_visits), "\n")
+    cat("Number of DFS OUT visits:", length(dfs_out_visits), "\n")
+    if (length(dfs_in_visits) > 0) {
+      cat("First IN visit:\n")
+      print(dfs_in_visits[[1]])
+    }
+  })
+
+  # Structured tests
+  expect_equal(length(dfs_in_visits), 10)
+  expect_equal(length(dfs_out_visits), 10)
+})
+
 # motifs_randesu_callback_closure_impl
 
 test_that("motifs_randesu_callback_closure_impl basic", {
