74c944bb94d61d7904295bff9d06779be0955b8e
Maëlle Salmon <maelle.salmon@yahoo.se>
2026-07-03 14:58:18 +0200

    test: fix expectation

--- tests/testthat/test-cycles.R
+++ tests/testthat/test-cycles.R
@@ -27,7 +27,10 @@ test_that("simple_cycle() works directed", {
 })
 
 test_that("simple_cycle() works undirected", {
-  g <- graph_from_literal(A -+ B -+ C -+ A -+ D -+ E +- F -+ A, E -+ E, A -+ F, simplify = FALSE)
+  g <- graph_from_literal(
+    A -+ B -+ C -+ A -+ D -+ E +- F -+ A, E -+ E, A -+ F, 
+    simplify = FALSE
+  )
   all_simple_cycles <- simple_cycles(g, mode = "all")
   expect_length(all_simple_cycles$vertices, 5)
   expect_length(all_simple_cycles$edges, 5)
@@ -40,8 +43,8 @@ test_that("simple_cycle() works undirected", {
   expect_equal(as.numeric(all_simple_cycles$vertices[[2]]), c(1, 4, 5, 6))
 
   all_simple_cycles_23 <- simple_cycles(g, mode = "all", min = 2, max = 3)
-  expect_length(all_simple_cycles$vertices, 5)
-  expect_length(all_simple_cycles$edges, 5)
+  expect_length(all_simple_cycles_23$vertices, 2)
+  expect_length(all_simple_cycles_23$edges, 2)
 })
 
 # Tests for callback function
